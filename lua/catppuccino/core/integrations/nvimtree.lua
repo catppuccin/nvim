0f07829c2ed43864a355ce59b60cec7c682a3cc2
Pocco81 <pocco451@gmail.com>
2021-08-19 22:45:17 -0500

    fix: better colors for nvim tree

--- lua/catppuccino/core/integrations/nvimtree.lua
+++ lua/catppuccino/core/integrations/nvimtree.lua
@@ -1,16 +1,19 @@
 return {
-    NvimTreeNormal = {fg = cpt.fg_sidebar, bg = cpt.bg_sidebar},
-    NvimTreeFolderIcon = {fg = cpt.comment},
-    NvimTreeRootFolder = {fg = cpt.orange, style = "bold"},
-    NvimTreeSymlink = {fg = cpt.magenta},
     NvimTreeFolderName = {fg = cpt.blue},
-    NvimTreeEmptyFolderName = {fg = cpt.comment},
-    NvimTreeOpenedFolderName = {fg = cpt.blue_br},
-    NvimTreeOpenedFile = {fg = cpt.magenta},
+    NvimTreeFolderIcon = {fg = cpt.blue},
+    NvimTreeNormal = {fg = cpt.fg_sidebar, bg = cpt.bg_sidebar},
+    NvimTreeOpenedFolderName = {fg = cpt.blue},
+    NvimTreeEmptyFolderName = {fg = cpt.blue_br},
+    NvimTreeIndentMarker = {fg = cpt.comment},
+	NvimTreeVertSplit = {fg = cpt.black, bg = cpt.black},
+    NvimTreeRootFolder = {fg = cpt.black, style = "bold"},
+    NvimTreeSymlink = {fg = cpt.magenta},
+	NvimTreeStatuslineNc = {fg = cpt.black, bg = cpt.black},
     NvimTreeGitDirty = {fg = cpt.git.change},
     NvimTreeGitNew = {fg = cpt.git.add},
     NvimTreeGitDeleted = {fg = cpt.git.delete},
     NvimTreeSpecialFile = {fg = cpt.cyan},
-    NvimTreeIndentMarker = {fg = cpt.fg_gutter},
     NvimTreeImageFile = {fg = cpt.fg_sidebar}
+
+    -- NvimTreeOpenedFile = {fg = cpt.magenta},
 }
