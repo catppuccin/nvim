0b9e1e36fc2c814a8887364be089dfea6d2ac0d0
Pocco81 <pocco451@gmail.com>
2021-08-20 13:52:03 -0500

    dev: added hooks

--- lua/catppuccino/init.lua
+++ lua/catppuccino/init.lua
@@ -1,7 +1,18 @@
 local M = {}
 
 function M.load()
+
+	local catppuccino = require("catppuccino")
+
+    if (catppuccino.before_loading ~= nil) then
+        catppuccino.before_loading()
+    end
+
     require("catppuccino.main").main("load")
+
+    if (catppuccino.after_loading ~= nil) then
+        catppuccino.after_loading()
+    end
 end
 
 function M.setup(custom_opts)
