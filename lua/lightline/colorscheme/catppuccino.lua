f276e38c1df97aff2e0b3f70a02244ef9661c9e4
Pocco81 <pocco451@gmail.com>
2021-08-20 20:54:41 -0500

    feat: lightline integration

---
+++ lua/lightline/colorscheme/catppuccino.lua
@@ -0,0 +1,37 @@
+local cpt = require("catppuccino.core.cs").get_color_scheme(require("catppuccino.config").options["cptcheme"])
+local catppuccino = {}
+
+catppuccino.normal = {
+    left = {{cpt.black, cpt.blue}, {cpt.blue, cpt.bg}},
+    middle = {{cpt.blue, cpt.fg_gutter}},
+    right = {{cpt.fg_sidebar, cpt.bg_statusline}, {cpt.blue, cpt.bg}},
+    error = {{cpt.black, cpt.error}},
+    warning = {{cpt.black, cpt.warning}}
+}
+
+catppuccino.insert = {
+    left = {{cpt.black, cpt.green}, {cpt.blue, cpt.bg}}
+}
+
+catppuccino.visual = {
+    left = {{cpt.black, cpt.magenta}, {cpt.blue, cpt.bg}}
+}
+
+catppuccino.replace = {
+    left = {{cpt.black, cpt.red}, {cpt.blue, cpt.bg}}
+}
+
+catppuccino.inactive = {
+    left = {{cpt.blue, cpt.bg_statusline}, {cpt.comment, cpt.bg}},
+    middle = {{cpt.fg_gutter, cpt.bg_statusline}},
+    right = {{cpt.fg_gutter, cpt.bg_statusline}, {cpt.comment, cpt.bg}}
+}
+
+catppuccino.tabline = {
+    left = {{cpt.comment, cpt.bg_highlight}, {cpt.comment, cpt.bg}},
+    middle = {{cpt.fg_gutter, cpt.bg_statusline}},
+    right = {{cpt.fg_gutter, cpt.bg_statusline}, {cpt.comment, cpt.bg}},
+    tabsel = {{cpt.blue, cpt.fg_gutter}, {cpt.comment, cpt.bg}}
+}
+
+return catppuccino
