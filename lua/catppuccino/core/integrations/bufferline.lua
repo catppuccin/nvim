cc54a4c68f4b4d449f08269afb12319e233879a0
Pocco81 <pocco451@gmail.com>
2021-08-20 23:12:21 -0500

    dev: fixed bufferline integration

--- lua/catppuccino/core/integrations/bufferline.lua
+++ lua/catppuccino/core/integrations/bufferline.lua
@@ -1,4 +1,22 @@
+local util = require("catppuccino.utils.util")
+
 return {
-    BufferLineIndicatorSelected = {fg = cpt.git.change},
-    BufferLineFill = {bg = cpt.black}
+    BufferLineFill = {bg = util.brighten(cpt.bg, 0.04)},
+    BufferLineBackground = {fg = cpt.gray, bg = cpt.black},
+    BufferLineBufferVisible = {fg = cpt.gray, bg = cpt.black},
+    BufferLineBufferSelected = {fg = cpt.white, bg = cpt.bg},
+    BufferLineTab = {fg = cpt.gray, bg = cpt.bg},
+    BufferLineTabSelected = {fg = cpt.red, bg = cpt.blue},
+    BufferLineTabClose = {fg = cpt.red, bg = cpt.black},
+    BufferLineIndicatorSelected = {fg = cpt.bg, bg = cpt.bg},
+
+	-- separators
+    BufferLineSeparator = {fg = cpt.black, bg = cpt.black},
+    BufferLineSeparatorVisible = {fg = cpt.black, bg = cpt.black},
+    BufferLineSeparatorSelected = {fg = cpt.black, bg = cpt.black},
+
+	-- close buttons
+    BufferLineCloseButton = {fg = cpt.gray, bg = cpt.black},
+    BufferLineCloseButtonVisible = {fg = cpt.gray, bg = cpt.black},
+    BufferLineCloseButtonSelected = {fg = cpt.red, bg = cpt.bg},
 }
