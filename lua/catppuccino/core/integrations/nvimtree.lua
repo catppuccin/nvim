f7cdbab0b2f32434b4244ca605b5fa241d2a95ac
Mat Jones <mat.jones@agilebits.com>
2021-08-25 09:46:46 -0400

    fix formatting again

--- lua/catppuccino/core/integrations/nvimtree.lua
+++ lua/catppuccino/core/integrations/nvimtree.lua
@@ -11,23 +11,24 @@ function M.get(cpt)
     then
       rootFolderColor = cpt.blue
     end
+
     return {
-      NvimTreeFolderName = {fg = cpt.blue},
-      NvimTreeFolderIcon = {fg = cpt.blue},
-      NvimTreeNormal = {fg = cpt.fg, bg = cpt.bg_sidebar},
-      NvimTreeOpenedFolderName = {fg = cpt.blue},
-      NvimTreeEmptyFolderName = {fg = cpt.blue_br},
-      NvimTreeIndentMarker = {fg = cpt.comment},
-      NvimTreeVertSplit = {fg = cpt.black, bg = cpt.black},
-      NvimTreeRootFolder = {fg = rootFolderColor, style = "bold"},
-      NvimTreeSymlink = {fg = cpt.magenta},
-      NvimTreeStatuslineNc = {fg = cpt.black, bg = cpt.black},
-      NvimTreeGitDirty = {fg = cpt.git.change},
-      NvimTreeGitNew = {fg = cpt.git.add},
-      NvimTreeGitDeleted = {fg = cpt.git.delete},
-      NvimTreeSpecialFile = {fg = cpt.cyan},
-      NvimTreeImageFile = {fg = cpt.fg_sidebar},
-      NvimTreeOpenedFile = {fg = cpt.magenta},
+        NvimTreeFolderName = {fg = cpt.blue},
+        NvimTreeFolderIcon = {fg = cpt.blue},
+        NvimTreeNormal = {fg = cpt.fg, bg = cpt.bg_sidebar},
+        NvimTreeOpenedFolderName = {fg = cpt.blue},
+        NvimTreeEmptyFolderName = {fg = cpt.blue_br},
+        NvimTreeIndentMarker = {fg = cpt.comment},
+        NvimTreeVertSplit = {fg = cpt.black, bg = cpt.black},
+        NvimTreeRootFolder = {fg = rootFolderColor, style = "bold"},
+        NvimTreeSymlink = {fg = cpt.magenta},
+        NvimTreeStatuslineNc = {fg = cpt.black, bg = cpt.black},
+        NvimTreeGitDirty = {fg = cpt.git.change},
+        NvimTreeGitNew = {fg = cpt.git.add},
+        NvimTreeGitDeleted = {fg = cpt.git.delete},
+        NvimTreeSpecialFile = {fg = cpt.cyan},
+        NvimTreeImageFile = {fg = cpt.fg_sidebar},
+        NvimTreeOpenedFile = {fg = cpt.magenta}
     }
 end
 
