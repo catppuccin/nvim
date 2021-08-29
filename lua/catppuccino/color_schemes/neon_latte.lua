4ec76d767146b6b4b6e367f7a6f68dbc45b2d83c
Pocco81 <pocco451@gmail.com>
2021-08-29 11:03:21 -0500

    dev (refactor): siplified git and diff relation in colorschemes

--- lua/catppuccino/color_schemes/neon_latte.lua
+++ lua/catppuccino/color_schemes/neon_latte.lua
@@ -27,12 +27,6 @@ local colors = {
 	orange_br = "#F6A878",
 	pink_br = "#DF97DB",
 	comment = "#526175",
-	git = {
-		add = "#dfdfe0",
-		change = "#F8B58C",
-		delete = "#e06c75",
-		conflict = "#FFE070",
-	},
 }
 
 util.bg = colors.bg
@@ -40,25 +34,23 @@ colors.bg_alt = util.darken(colors.bg, 0.75, "#000000")
 colors.bg_highlight = util.brighten(colors.bg, 0.05)
 colors.fg_alt = util.darken(colors.fg, 0.80, "#000000")
 
-colors.diff = { -- also used for gitsigns
-	add = util.darken(colors.blue, 0.50),
-	delete = util.darken(colors.red, 0.50),
-	change = util.darken(colors.yellow, 0.50),
+colors.diff = { -- git and native diffs
+	add = colors.blue,
+	change = colors.yellow,
+	delete = colors.red,
 	text = colors.blue,
+	conflict = colors.magenta,
 }
 
-colors.git.ignore = colors.black
 colors.black = util.darken(colors.bg, 0.8, "#000000")
 colors.border_highlight = colors.blue
 colors.border = colors.black
 
--- Popups and statusline always get a dark background
+-- Popups and statusline should always be darker
 colors.bg_popup = colors.bg_alt
 colors.bg_statusline = colors.bg_alt
 
--- Sidebar and Floats are configurable
--- colors.bg_sidebar = config.darkSidebar and colors.bg_alt or colors.bg
--- colors.bg_float = config.darkFloat and colors.bg_alt or colors.bg
+-- Sidebar and Floats
 colors.bg_sidebar = colors.bg_alt
 colors.bg_float = colors.bg_alt
 
