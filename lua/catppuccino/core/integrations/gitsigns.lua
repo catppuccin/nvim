418095d52aa60d482c581a1b3901ee97510a9947
Pocco81 <pocco451@gmail.com>
2021-09-01 22:04:19 -0500

    fix #17: Gitsigns Backgrounds Sometimes Not Transparent

--- lua/catppuccino/core/integrations/gitsigns.lua
+++ lua/catppuccino/core/integrations/gitsigns.lua
@@ -1,11 +1,10 @@
 local M = {}
-local util = require("catppuccino.utils.util")
 
 function M.get(cpt)
 	return {
-		GitSignsAdd = { fg = util.darken(cpt.diff.add, 0.50), bg = cpt.bg }, -- diff mode: Added line |diff.txt|
-		GitSignsChange = { fg = util.darken(cpt.diff.change, 0.50), bg = cpt.bg }, -- diff mode: Changed line |diff.txt|
-		GitSignsDelete = { fg = util.darken(cpt.diff.delete, 0.50), bg = cpt.bg }, -- diff mode: Deleted line |diff.txt|
+		GitSignsAdd = { fg = cpt.diff.add, bg = cpc.transparency and cpt.none or cpt.bg }, -- diff mode: Added line |diff.txt|
+		GitSignsChange = { fg = cpt.diff.change, bg = cpc.transparency and cpt.none or cpt.bg }, -- diff mode: Changed line |diff.txt|
+		GitSignsDelete = { fg = cpt.diff.remove, bg = cpc.transparency and cpt.none or cpt.bg }, -- diff mode: Deleted line |diff.txt|
 	}
 end
 
