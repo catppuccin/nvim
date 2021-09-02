b69511f78e6f7195f5b72cb6051ad61a186357f4
Pocco81 <pocco451@gmail.com>
2021-09-02 14:13:58 -0500

    fix #22: Contrast on bufferline tabs

--- lua/catppuccino/core/integrations/bufferline.lua
+++ lua/catppuccino/core/integrations/bufferline.lua
@@ -1,24 +1,26 @@
+local M = {}
 local util = require("catppuccino.utils.util")
 
-local M = {}
-
 function M.get(cpt)
+
+	local inactive_clr = util.darken("#000000", 0.56)
+
 	return {
-		BufferLineFill = { bg = util.brighten(cpt.bg, 0.04) },
-		BufferLineBackground = { fg = cpt.gray, bg = cpt.black },
-		BufferLineBufferVisible = { fg = cpt.gray, bg = cpt.black },
+		BufferLineFill = { bg = util.brighten(cpt.bg, 0.05) },
+		BufferLineBackground = { fg = cpt.gray, bg = inactive_clr },
+		BufferLineBufferVisible = { fg = cpt.gray, bg = inactive_clr },
 		BufferLineBufferSelected = { fg = cpt.fg, bg = cpt.bg },
 		BufferLineTab = { fg = cpt.gray, bg = cpt.bg },
 		BufferLineTabSelected = { fg = cpt.red, bg = cpt.blue },
-		BufferLineTabClose = { fg = cpt.red, bg = cpt.black },
+		BufferLineTabClose = { fg = cpt.red, bg = inactive_clr },
 		BufferLineIndicatorSelected = { fg = cpt.bg, bg = cpt.bg },
 		-- separators
-		BufferLineSeparator = { fg = cpt.black, bg = cpt.black },
-		BufferLineSeparatorVisible = { fg = cpt.black, bg = cpt.black },
-		BufferLineSeparatorSelected = { fg = cpt.black, bg = cpt.black },
+		BufferLineSeparator = { fg = inactive_clr, bg = inactive_clr },
+		BufferLineSeparatorVisible = { fg = inactive_clr, bg = inactive_clr },
+		BufferLineSeparatorSelected = { fg = inactive_clr, bg = inactive_clr },
 		-- close buttons
-		BufferLineCloseButton = { fg = cpt.gray, bg = cpt.black },
-		BufferLineCloseButtonVisible = { fg = cpt.gray, bg = cpt.black },
+		BufferLineCloseButton = { fg = cpt.gray, bg = inactive_clr },
+		BufferLineCloseButtonVisible = { fg = cpt.gray, bg = inactive_clr },
 		BufferLineCloseButtonSelected = { fg = cpt.red, bg = cpt.bg },
 	}
 end
