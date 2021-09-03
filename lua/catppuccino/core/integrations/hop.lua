c6e5f4a7ea29851de04b10c6bf8e9a7f7082bf04
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-09-03 09:12:09 +0530

    Define hop colors

---
+++ lua/catppuccino/core/integrations/hop.lua
@@ -0,0 +1,14 @@
+local util = require("catppuccino.utils.util")
+
+local M = {}
+
+function M.get(cpt)
+	return {
+		HopNextKey = {bg = cpt.bg, fg = cpt.orange, style = "bold"},
+		HopNextKey1 = {bg = cpt.bg, fg = cpt.blue, style = "bold"},
+		HopNextKey2 = {bg = cpt.bg, fg = util.darken(cpt.blue, 0.80, cpt.bg)},
+		HopUnmatched = {bg = cpt.bg, fg = cpt.comment},
+	}
+end
+
+return M
