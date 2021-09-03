a5a3d62f73eb2fe98b6353b62f26d51109c5e289
Pocco81 <pocco451@gmail.com>
2021-09-03 06:59:11 -0500

    fix: made hop colors more visible and made them stand out more

--- lua/catppuccino/core/integrations/hop.lua
+++ lua/catppuccino/core/integrations/hop.lua
@@ -1,12 +1,10 @@
-local util = require("catppuccino.utils.util")
-
 local M = {}
 
 function M.get(cpt)
 	return {
-		HopNextKey = { bg = cpt.bg, fg = cpt.orange, style = "bold" },
+		HopNextKey = { bg = cpt.bg, fg = cpt.orange, style = "bold,underline" },
 		HopNextKey1 = { bg = cpt.bg, fg = cpt.blue, style = "bold" },
-		HopNextKey2 = { bg = cpt.bg, fg = util.darken(cpt.blue, 0.80, cpt.bg) },
+		HopNextKey2 = { bg = cpt.bg, fg = cpt.green, style = "bold,italic" },
 		HopUnmatched = { bg = cpt.bg, fg = cpt.comment },
 	}
 end
