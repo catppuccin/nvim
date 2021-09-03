e04c0b39b74ebfc75373329936001d8a4f5a0110
Pocco81 <pocco451@gmail.com>
2021-09-02 23:33:02 -0500

    fix: formatted hop integ

--- lua/catppuccino/core/integrations/hop.lua
+++ lua/catppuccino/core/integrations/hop.lua
@@ -4,10 +4,10 @@ local M = {}
 
 function M.get(cpt)
 	return {
-		HopNextKey = {bg = cpt.bg, fg = cpt.orange, style = "bold"},
-		HopNextKey1 = {bg = cpt.bg, fg = cpt.blue, style = "bold"},
-		HopNextKey2 = {bg = cpt.bg, fg = util.darken(cpt.blue, 0.80, cpt.bg)},
-		HopUnmatched = {bg = cpt.bg, fg = cpt.comment},
+		HopNextKey = { bg = cpt.bg, fg = cpt.orange, style = "bold" },
+		HopNextKey1 = { bg = cpt.bg, fg = cpt.blue, style = "bold" },
+		HopNextKey2 = { bg = cpt.bg, fg = util.darken(cpt.blue, 0.80, cpt.bg) },
+		HopUnmatched = { bg = cpt.bg, fg = cpt.comment },
 	}
 end
 
