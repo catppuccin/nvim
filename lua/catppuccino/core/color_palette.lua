25e38aaff8c75992281ab6e3297d83605b87e8eb
Pocco81 <pocco451@gmail.com>
2021-10-28 22:37:42 -0500

    feat: added initial 11-colors color palette as discussed in #44

---
+++ lua/catppuccino/core/color_palette.lua
@@ -0,0 +1,15 @@
+local color_palette = {
+	catppuccino0 = "#F2F2F7",		-- Ghost White
+	catppuccino1 = "#2C2E3D",		-- Raisin Black
+	catppuccino2 = "#16131F",		-- Xiketic
+	catppuccino3 = "#EA31B5",		-- Fashion Fuchsia
+	catppuccino4 = "#AE9AD6",		-- Blue Shell
+	catppuccino5 = "#F0A8E4",		-- Orchid Crayola
+	catppuccino6 = "#E28D8D",		-- New York Pink
+	catppuccino7 = "#E7A988",		-- Tumbleweed
+	catppuccino8 = "#ADD692",		-- Granny Smith Apple
+	catppuccino9 = "#E3DAA3",		-- Megium Champagne
+	catppuccino10 = "#7EA1C8",		-- Cerulean Frost
+}
+
+return color_palette
