274c426b4b002bb70118ac2bf87417869541abd3
Pocco81 <pocco451@gmail.com>
2021-10-31 22:10:26 -0500

    fix: bufferline integration

--- lua/catppuccino/core/integrations/bufferline.lua
+++ lua/catppuccino/core/integrations/bufferline.lua
@@ -7,10 +7,10 @@ function M.get(cp)
 
 	return {
 		BufferLineFill = { bg = util.brighten(cp.catppuccino2, 0.05) },
-		BufferLineBackground = { fg = cp.catppuccino1, bg = inactive_clr },
-		BufferLineBufferVisible = { fg = cp.catppuccino1, bg = inactive_clr },
+		BufferLineBackground = { fg = cp.catppuccino13, bg = inactive_clr },
+		BufferLineBufferVisible = { fg = cp.catppuccino13, bg = inactive_clr },
 		BufferLineBufferSelected = { fg = cp.catppuccino11, bg = cp.catppuccino2 },
-		BufferLineTab = { fg = cp.catppuccino1, bg = cp.catppuccino2 },
+		BufferLineTab = { fg = cp.catppuccino13, bg = cp.catppuccino2 },
 		BufferLineTabSelected = { fg = cp.catppuccino6, bg = cp.catppuccino10 },
 		BufferLineTabClose = { fg = cp.catppuccino6, bg = inactive_clr },
 		BufferLineIndicatorSelected = { fg = cp.catppuccino2, bg = cp.catppuccino2 },
@@ -19,8 +19,8 @@ function M.get(cp)
 		BufferLineSeparatorVisible = { fg = inactive_clr, bg = inactive_clr },
 		BufferLineSeparatorSelected = { fg = inactive_clr, bg = inactive_clr },
 		-- close buttons
-		BufferLineCloseButton = { fg = cp.catppuccino1, bg = inactive_clr },
-		BufferLineCloseButtonVisible = { fg = cp.catppuccino1, bg = inactive_clr },
+		BufferLineCloseButton = { fg = cp.catppuccino13, bg = inactive_clr },
+		BufferLineCloseButtonVisible = { fg = cp.catppuccino13, bg = inactive_clr },
 		BufferLineCloseButtonSelected = { fg = cp.catppuccino6, bg = cp.catppuccino2 },
 	}
 end
