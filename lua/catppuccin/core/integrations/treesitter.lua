2d4e0de3e13848da2c0dfab33c19bca08b24658d
Pocco81 <pocco451@gmail.com>
2021-11-18 20:03:38 -0500

    changed the way highlights were implemented

--- lua/catppuccin/core/integrations/treesitter.lua
+++ lua/catppuccin/core/integrations/treesitter.lua
@@ -26,7 +26,7 @@ function M.get(cp)
 		TSField = { fg = cp.catppuccin5 }, -- For fields.
 		-- rustTSField = { fg = cp.catppuccin12 }, -- For fields.
 		-- TSFloat             = { };    -- For floats.
-		TSFunction = { fg = cp.catppuccin9, style = cnf.styles.functions }, -- For function (calls and definitions).
+		TSFunction = { fg = cp.catppuccin2, style = cnf.styles.functions }, -- For function (calls and definitions).
 		TSFuncBuiltin = { fg = cp.catppuccin2 }, -- For builtin functions: `table.insert` in Lua.
 		TSFuncMacro = { fg = cp.catppuccin5 }, -- For macro defined functions (calls and definitions): each `macro_rules` in Ruscp.
 		TSInclude = { fg = cp.catppuccin4, style = cnf.styles.keywords }, -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
@@ -35,15 +35,15 @@ function M.get(cp)
 		TSKeywordOperator = { fg = cp.catppuccin2, style = cnf.styles.keywords }, -- For `new` keyword operator
 		TSLabel = { fg = cp.catppuccin9 }, -- For labels: `label:` in C and `:label:` in Lua.
 		-- TSMethod            = { };    -- For method calls and definitions.
-		TSNamespace = { fg = cp.catppuccin2 }, -- For identifiers referring to modules and namespaces.
+		TSNamespace = { fg = cp.catppuccin3, style = "italic" }, -- For identifiers referring to modules and namespaces.
 		-- TSNone              = { };    -- TODO: docs
 		-- TSNumber            = { };    -- For all numbers
 		TSOperator = { fg = cp.catppuccin4 }, -- For any operator: `+`, but also `->` and `*` in cp.
-		TSParameter = { fg = cp.catppuccin6 }, -- For parameters of a function.
+		TSParameter = { fg = cp.catppuccin9, style = "italic" }, -- For parameters of a function.
 		-- TSParameterReference= { };    -- For references to parameters of a function.
 		TSProperty = { fg = cp.catppuccin5 }, -- Same as `TSField`.
 		tomlTSProperty = { fg = cp.catppuccin9 }, -- Differentiates between string and properties
-		TSPunctDelimiter = { fg = cp.catppuccin2 }, -- For delimiters ie: `.`
+		TSPunctDelimiter = { fg = cp.catppuccin11 }, -- For delimiters ie: `.`
 		TSPunctBracket = { fg = cp.catppuccin11 }, -- For brackets and parenthesis.
 		TSPunctSpecial = { fg = cp.catppuccin0 }, -- For special punctutation that does not fall in the catagories before.
 		TSRepeat = { fg = cp.catppuccin5, style = cnf.styles.keywords }, -- For keywords related to loops.
@@ -52,7 +52,7 @@ function M.get(cp)
 		TSStringEscape = { fg = cp.catppuccin4, style = cnf.styles.strings }, -- For escape characters within a string.
 		-- TSSymbol            = { };    -- For identifiers referring to symbols or atoms.
 		-- TSType              = { };    -- For types.
-		TSTypeBuiltin = { fg = cp.catppuccin2, style = cnf.styles.keywords }, -- For builtin types.
+		TSTypeBuiltin = { fg = cp.catppuccin6, style = cnf.styles.keywords }, -- For builtin types.
 		TSVariable = { style = cnf.styles.variables }, -- Any variable name that does not have another highlighcp.
 		TSVariableBuiltin = { fg = cp.catppuccin5, style = cnf.styles.keywords }, -- Variable names that are defined by the languages, like `this` or `self`.
 		-- TSTag               = { };    -- Tags like html tag names.
