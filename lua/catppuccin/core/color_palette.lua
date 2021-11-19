2ce3f61c87209e7794ee6887fab7d4e6c821afeb
Pocco81 <pocco451@gmail.com>
2021-11-18 20:02:56 -0500

    replaced cyan with yellow

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,19 +1,19 @@
 local color_palette = {
 	catppuccin0 = "#faf8f6",		-- Ghost White
-	catppuccin1 = "#201D29",		-- Xiketic
+	catppuccin1 = "#1E1B28",		-- Xiketic
 	catppuccin2 = "#ECBFBD",		-- Fashion Fuchsia
 	catppuccin3 = "#B6A1DD",		-- Blue Shell
 	catppuccin4 = "#E9A3DA",		-- Orchid Crayola
 	catppuccin5 = "#E28C8C",		-- New York Pink
 	catppuccin6 = "#F6C29C",		-- Tumbleweed
 	catppuccin7 = "#C2E5B3",		-- Granny Smith Apple
-	catppuccin8 = "#A4D8D8",		-- Megium Champagne
+	catppuccin8 = "#E5DBA0",		-- Megium Champagne
 	catppuccin9 = "#779ecb",		-- Cerulean Frost
 	catppuccin10 = "#B9C4E3",		-- Periwinkle Crayola
 	catppuccin11 = "#6F6A8A",		-- Rhythm
 	catppuccin12 = "#3e4058",		-- Independence
 	catppuccin13 = "#15121C",		-- Xiketic
-	catppuccin14 = "#221E2F",		-- Raisin Black
+	catppuccin14 = "#2C2837",		-- Raisin Black
 	catppuccin15 = "#312C44",		-- Space Cadet
 }
 
