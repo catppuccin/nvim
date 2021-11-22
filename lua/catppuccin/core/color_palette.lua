9acd16681de6efa54f9f4047dd545a295fc835d6
Pocco81 <pocco451@gmail.com>
2021-11-21 21:30:51 -0500

    better flamingo and darkest black

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -3,7 +3,7 @@ local color_palette = {
 	catppuccin1 = "#1E1B28",		-- Xiketic
 	catppuccin2 = "#ECBFBD",		-- Fashion Fuchsia
 	catppuccin3 = "#B6A1DD",		-- Blue Shell
-	catppuccin4 = "#E9A3DA",		-- Orchid Crayola
+	catppuccin4 = "#F0AFE1",
 	catppuccin5 = "#E28C8C",		-- New York Pink
 	catppuccin6 = "#F6C29C",		-- Tumbleweed
 	catppuccin7 = "#C2E5B3",		-- Granny Smith Apple
@@ -12,7 +12,7 @@ local color_palette = {
 	catppuccin10 = "#B9C4E3",		-- Periwinkle Crayola
 	catppuccin11 = "#6F6A8A",		-- Rhythm
 	catppuccin12 = "#3e4058",		-- Independence
-	catppuccin13 = "#15121C",		-- Xiketic
+	catppuccin13 = "#16131D",
 	catppuccin14 = "#2C2837",		-- Raisin Black
 	catppuccin15 = "#312C44",		-- Space Cadet
 }
