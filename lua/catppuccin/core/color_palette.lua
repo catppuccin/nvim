d9577cc1c105adc98427a87303c435cbbdfcfdec
Pocco81 <pocco451@gmail.com>
2021-11-26 17:41:45 -0500

    improved contrast

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,20 +1,20 @@
 local color_palette = {
-	catppuccin0 = "#faf8f6",		-- Ghost White
-	catppuccin1 = "#1E1B28",		-- Xiketic
+	catppuccin0 = "#C2E7F0",		-- Ghost White
+	catppuccin1 = "#201F2A",		-- Xiketic
 	catppuccin2 = "#ECBFBD",		-- Fashion Fuchsia
 	catppuccin3 = "#B6A1DD",		-- Blue Shell
 	catppuccin4 = "#F0AFE1",
 	catppuccin5 = "#E28C8C",		-- New York Pink
-	catppuccin6 = "#F6C29C",		-- Tumbleweed
-	catppuccin7 = "#C2E5B3",		-- Granny Smith Apple
-	catppuccin8 = "#E5DBA0",		-- Megium Champagne
-	catppuccin9 = "#88B8EA",		-- Cerulean Frost
-	catppuccin10 = "#B9C4E3",		-- Periwinkle Crayola
-	catppuccin11 = "#6F6A8A",		-- Rhythm
+	catppuccin6 = "#F7C196",		-- Tumbleweed
+	catppuccin7 = "#B3E3A6",		-- Granny Smith Apple
+	catppuccin8 = "#E6DDAC",		-- Megium Champagne
+	catppuccin9 = "#6A98DB",		-- Cerulean Frost
+	catppuccin10 = "#D7DAE0",		-- Periwinkle Crayola
+	catppuccin11 = "#7B788A",		-- Rhythm
 	catppuccin12 = "#3e4058",		-- Independence
 	catppuccin13 = "#16131D",
-	catppuccin14 = "#2C2837",		-- Raisin Black
-	catppuccin15 = "#312C44",		-- Space Cadet
+	catppuccin14 = "#1C1924",		-- Raisin Black
+	catppuccin15 = "#2D293B",		-- Space Cadet
 }
 
 return color_palette
