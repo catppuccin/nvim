7b05ef6e63875e0e2197c784ed1fd9399014b203
Pocco81 <pocco451@gmail.com>
2021-12-14 05:17:51 -0500

    fix: remove names from color palette because they were misleading

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,20 +1,20 @@
 local color_palette = {
-	catppuccin0 = "#9DDDCB",		-- Ghost White
-	catppuccin1 = "#1E1E28",    -- Xiketic
-	catppuccin2 = "#ECBFBD",		-- Fashion Fuchsia
-	catppuccin3 = "#C6AAE8",		-- Blue Shell
+	catppuccin0 = "#9DDDCB",
+	catppuccin1 = "#1E1E28",
+	catppuccin2 = "#ECBFBD",
+	catppuccin3 = "#C6AAE8",
 	catppuccin4 = "#F0AFE1",
-	catppuccin5 = "#E28C8C",		-- New York Pink
-	catppuccin6 = "#F7C196",		-- Tumbleweed
-	catppuccin7 = "#B3E1A3",		-- Granny Smith Apple
-	catppuccin8 = "#EADDA0",		-- Megium Champagne
-	catppuccin9 = "#A4B9EF",		-- Cerulean Frost
-	catppuccin10 = "#D7DAE0",		-- Periwinkle Crayola
-	catppuccin11 = "#6E6C7C",		-- Rhythm
-	catppuccin12 = "#3e4058",		-- Independence
+	catppuccin5 = "#E28C8C",
+	catppuccin6 = "#F7C196",
+	catppuccin7 = "#B3E1A3",
+	catppuccin8 = "#EADDA0",
+	catppuccin9 = "#A4B9EF",
+	catppuccin10 = "#D7DAE0",
+	catppuccin11 = "#6E6C7C",
+	catppuccin12 = "#3e4058",
 	catppuccin13 = "#15121C",
-	catppuccin14 = "#1B1923",		-- Raisin Black
-	catppuccin15 = "#2D293B",		-- Space Cadet
+	catppuccin14 = "#1B1923",
+	catppuccin15 = "#2D293B",
 }
 
 return color_palette
