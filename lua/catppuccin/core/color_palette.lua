3a5b6bbbe91b72eafda19c700dd61d1556240f39
Pocco81 <pocco451@gmail.com>
2021-12-16 21:35:03 -0500

    BREAKING CHANGE: color palette

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,20 +1,25 @@
 local color_palette = {
-	catppuccin0 = "#9DDDCB",
-	catppuccin1 = "#1E1E28",
-	catppuccin2 = "#ECBFBD",
-	catppuccin3 = "#C6AAE8",
-	catppuccin4 = "#F0AFE1",
+	-- morning palette
+	catppuccin0 = "#9FDECD",
+	catppuccin2 = "#F0C8CC",
+	catppuccin3 = "#D0A6EB",
+	catppuccin4 = "#F1B4E3",
 	catppuccin5 = "#E28C8C",
 	catppuccin6 = "#F7C196",
-	catppuccin7 = "#B3E1A3",
+	catppuccin7 = "#B2E1A3",
 	catppuccin8 = "#EADDA0",
 	catppuccin9 = "#A4B9EF",
-	catppuccin10 = "#D7DAE0",
-	catppuccin11 = "#6E6C7C",
-	catppuccin12 = "#3e4058",
+
+	-- night palette
 	catppuccin13 = "#15121C",
 	catppuccin14 = "#1B1923",
-	catppuccin15 = "#2D293B",
+	catppuccin1 = "#1E1E28",
+	catppuccin16 = "#332E41",
+	catppuccin12 = "#575268",
+	catppuccin11 = "#6E6C7E",
+	catppuccin17 = "#988BA2",
+	catppuccin15 = "#C3BAC6",
+	catppuccin10 = "#D9DCE2",
 }
 
 return color_palette
