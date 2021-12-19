dfc4eece830d68a40ea5cfd3d5a87af944a6974f
Pocco81 <pocco451@gmail.com>
2021-12-19 09:39:43 -0500

    updated bufferline

--- lua/catppuccin/core/integrations/bufferline.lua
+++ lua/catppuccin/core/integrations/bufferline.lua
@@ -2,24 +2,24 @@ local M = {}
 
 function M.get(cp)
 
-	local inactive_clr = cp.catppuccin14
+	local inactive_bg = cp.catppuccin14
 
 	return {
 		BufferLineFill = { bg = cp.catppuccin13 },
-		BufferLineBackground = { fg = cp.catppuccin10, bg = inactive_clr },
-		BufferLineBufferVisible = { fg = cp.catppuccin12, bg = inactive_clr },
-		BufferLineBufferSelected = { fg = cp.catppuccin0, bg = cp.catppuccin1, style = "bold,italic" },
+		BufferLineBackground = { fg = cp.catppuccin10, bg = inactive_bg }, -- others
+		BufferLineBufferVisible = { fg = cp.catppuccin12, bg = inactive_bg },
+		BufferLineBufferSelected = { fg = cp.catppuccin10, bg = cp.catppuccin1, style = "bold,italic" }, -- current
 		BufferLineTab = { fg = cp.catppuccin12, bg = cp.catppuccin1 },
 		BufferLineTabSelected = { fg = cp.catppuccin5, bg = cp.catppuccin9 },
-		BufferLineTabClose = { fg = cp.catppuccin5, bg = inactive_clr },
+		BufferLineTabClose = { fg = cp.catppuccin5, bg = inactive_bg },
 		BufferLineIndicatorSelected = { fg = cp.catppuccin6, bg = cp.catppuccin1 },
 		-- separators
-		BufferLineSeparator = { fg = inactive_clr, bg = inactive_clr },
-		BufferLineSeparatorVisible = { fg = inactive_clr, bg = inactive_clr },
-		BufferLineSeparatorSelected = { fg = inactive_clr, bg = inactive_clr },
+		BufferLineSeparator = { fg = inactive_bg, bg = inactive_bg },
+		BufferLineSeparatorVisible = { fg = inactive_bg, bg = inactive_bg },
+		BufferLineSeparatorSelected = { fg = inactive_bg, bg = inactive_bg },
 		-- close buttons
-		BufferLineCloseButton = { fg = cp.catppuccin12, bg = inactive_clr },
-		BufferLineCloseButtonVisible = { fg = cp.catppuccin12, bg = inactive_clr },
+		BufferLineCloseButton = { fg = cp.catppuccin12, bg = inactive_bg },
+		BufferLineCloseButtonVisible = { fg = cp.catppuccin12, bg = inactive_bg },
 		BufferLineCloseButtonSelected = { fg = cp.catppuccin5, bg = cp.catppuccin1 },
 	}
 end
