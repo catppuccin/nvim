263048b150df8d83b992c86809ee46aa5c0baf74
Pocco81 <pocco451@gmail.com>
2021-12-21 14:30:36 -0500

    improved style of current selection

--- lua/catppuccin/core/integrations/telescope.lua
+++ lua/catppuccin/core/integrations/telescope.lua
@@ -4,7 +4,7 @@ function M.get(cp)
 	return {
 		TelescopeBorder = { fg = cp.catppuccin9 },
 		TelescopeSelectionCaret = { fg = cp.catppuccin2 },
-		TelescopeSelection = { fg = cp.catppuccin2, bg = cp.catppuccin16 },
+		TelescopeSelection = { fg = cp.catppuccin10, bg = cp.catppuccin16, style = "bold" },
 		TelescopeMatching = { fg = cp.catppuccin9 },
 	}
 end
