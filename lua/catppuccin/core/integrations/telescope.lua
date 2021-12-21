5fdcd9484b2c60ca390b9daccdbbf3b8cc6d8ade
Pocco81 <pocco451@gmail.com>
2021-12-21 14:27:11 -0500

    fix: softer telescope highlight

--- lua/catppuccin/core/integrations/telescope.lua
+++ lua/catppuccin/core/integrations/telescope.lua
@@ -4,7 +4,7 @@ function M.get(cp)
 	return {
 		TelescopeBorder = { fg = cp.catppuccin9 },
 		TelescopeSelectionCaret = { fg = cp.catppuccin2 },
-		TelescopeSelection = { fg = cp.catppuccin2, bg = cp.catppuccin15 },
+		TelescopeSelection = { fg = cp.catppuccin2, bg = cp.catppuccin16 },
 		TelescopeMatching = { fg = cp.catppuccin9 },
 	}
 end
