499a50904870c2b60037b29430a5a4d88b139754
Pocco81 <pocco451@gmail.com>
2021-12-28 12:12:56 -0500

    experiment 5 - nice results as well

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,24 +1,29 @@
 local color_palette = {
+	-- morning palette
+	catppuccin21 = "#F5E0DC",
 	catppuccin2 = "#F1CBCB", -- Flamingo
 	catppuccin3 = "#C5A8E7", -- Magenta
 	catppuccin4 = "#E2B2E2", -- Pink
 	catppuccin5 = "#E38C8F", -- Red
 	catppuccin19 = "#E995AF", -- Maroon
 	catppuccin6 = "#F1B790", -- Peach
-	catppuccin8 = "#F1D69C", -- Yellow
+	catppuccin8 = "#ECDDA9", -- Yellow
 	catppuccin0 = "#B1E1A6", -- Green
-	catppuccin18 = "#A4E5D1", -- Teal
+	catppuccin7 = "#A4E5D1", -- Teal
+	catppuccin20 = "#9AC3D5",
 	catppuccin9 = "#A3B9EF", -- Blue
-	catppuccin7 = "#CACCFF", -- Sky
-	catppuccin10 = "#DCDCE8", -- White
-	catppuccin15 = "#C3BAC6", -- Gray2
-	catppuccin17 = "#988BA2", -- Gray1
-	catppuccin11 = "#6E6C7E", -- Gray0
-	catppuccin12 = "#575268", -- Black4
-	catppuccin16 = "#332E41", -- Black3
-	catppuccin1 = "#1E1E28", -- Black2
-	catppuccin14 = "#1B1923", -- Black1
+	catppuccin18 = "#CACCFF", -- Sky
+
+	-- night palette
 	catppuccin13 = "#15121C", -- Black0
+	catppuccin14 = "#1B1923", -- Black1
+	catppuccin1 = "#1E1E28", -- Black2
+	catppuccin16 = "#332E41", -- Black3
+	catppuccin12 = "#575268", -- Black4
+	catppuccin11 = "#6E6C7E", -- Gray0
+	catppuccin17 = "#988BA2", -- Gray1
+	catppuccin15 = "#C3BAC6", -- Gray2
+	catppuccin10 = "#DCDCE8", -- White
 }
 
 return color_palette
--- lua/catppuccin/core/integrations/treesitter.lua
+++ lua/catppuccin/core/integrations/treesitter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.get(cp)
 	local keywords = cp.catppuccin4
 	local operators = cp.catppuccin19
-	local math_logic = cp.catppuccin7
+	local math_logic = cp.catppuccin18
 	return {
 		-- These groups are for the neovim tree-sitter highlights.
 		-- As of writing, tree-sitter support is a WIP, group names may change.
@@ -12,33 +12,33 @@ function M.get(cp)
 		-- you explicitly want to support Treesitter's improved syntax awareness.
 		TSField = { fg = cp.catppuccin2 }, -- For fields.
 		TSProperty = { fg = cp.catppuccin8, style = "italic" }, -- Same as TSField.
-		TSInclude = { fg = cp.catppuccin5, style = "italic" }, -- For includes: #include in C, use or extern crate in Rust, or require in Lua.
+		TSInclude = { fg = cp.catppuccin19, style = "italic" }, -- For includes: #include in C, use or extern crate in Rust, or require in Lua.
 		TSOperator = { fg = operators, style = "bold" }, -- For any operator: +, but also -> and * in cp.
 		TSKeywordOperator = { fg = operators, style = "bold" }, -- For new keyword operator
 		TSPunctSpecial = { fg = cp.catppuccin5 }, -- For special punctutation that does not fall in the catagories before.
 
-		TSFloat = { fg = math_logic, style = "italic" }, -- For floats.
-		TSNumber = { fg = math_logic, style = "italic" }, -- For all numbers
-		TSBoolean = { fg = math_logic, style = "italic" }, -- For booleans.
+		TSFloat = { fg = math_logic, style = "bold,italic" }, -- For floats.
+		TSNumber = { fg = math_logic, style = "bold,italic" }, -- For all numbers
+		TSBoolean = { fg = math_logic, style = "bold,italic" }, -- For booleans.
 
-		TSConstructor = { fg = cp.catppuccin9 }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
-		TSConstant = { fg = cp.catppuccin2, style = "NONE" }, -- For constants
+		TSConstructor = { fg = cp.catppuccin18 }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
+		TSConstant = { fg = cp.catppuccin6 }, -- For constants
 		TSConditional = { fg = keywords, style = "bold" }, -- For keywords related to conditionnals.
 		TSRepeat = { fg = keywords, style = "bold" }, -- For keywords related to loops.
-		TSException = { fg = cp.catppuccin6, style = "italic" }, -- For exception related keywords.
-		TSKeywordReturn = { fg = cp.catppuccin5 },
+		TSException = { fg = cp.catppuccin6, style = cnf.styles.keywords }, -- For exception related keywords.
 
 		-- builtin
-		TSConstBuiltin = { fg = cp.catppuccin18, style = cnf.styles.keywords }, -- For constant that are built in the language: nil in Lua.
-		TSFuncBuiltin = { fg = cp.catppuccin6, style = "italic" }, -- For builtin functions: table.insert in Lua.
-		TSTypeBuiltin = { fg = cp.catppuccin18, style = "italic" }, -- For builtin types.
+		TSConstBuiltin = { fg = cp.catppuccin20, style = cnf.styles.keywords }, -- For constant that are built in the language: nil in Lua.
+		TSFuncBuiltin = { fg = cp.catppuccin7, style = "italic" }, -- For builtin functions: table.insert in Lua.
+		TSTypeBuiltin = { fg = cp.catppuccin5, style = "italic" }, -- For builtin types.
 		TSVariableBuiltin = { fg = cp.catppuccin5, style = "italic" }, -- Variable names that are defined by the languages, like this or self.
 
 		TSFunction = { fg = cp.catppuccin9, style = cnf.styles.functions }, -- For function (calls and definitions).
 		TSFuncMacro = { fg = cp.catppuccin5 }, -- For macro defined functions (calls and definitions): each macro_rules in Ruscp.
-		TSParameter = { fg = cp.catppuccin2, style = "italic" }, -- For parameters of a function.
+		TSParameter = { fg = cp.catppuccin21, style = "italic" }, -- For parameters of a function.
 		TSKeywordFunction = { fg = cp.catppuccin4, style = cnf.styles.keywords }, -- For keywords used to define a fuction.
 		TSKeyword = { fg = keywords, style = cnf.styles.keywords }, -- For keywords that don't fall in previous categories.
+		TSKeywordReturn = { fg = cp.catppuccin5 },
 		-- TSAnnotation        = { };    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
 		-- TSAttribute         = { };    -- (unstable) TODO: docs
 		-- TSCharacter         = { };    -- For characters.
@@ -62,20 +62,19 @@ function M.get(cp)
 		-- TSSymbol            = { };    -- For identifiers referring to symbols or atoms.
 		TSType = { fg = cp.catppuccin8 }, -- For types.
 		TSVariable = { fg = cp.catppuccin10, style = cnf.styles.variables }, -- Any variable name that does not have another highlighcp.
+		TSTagAttribute = { fg = cp.catppuccin3, style = "italic" }, -- Tags like html tag names.
+		TSTag = { fg = cp.catppuccin6 }, -- Tags like html tag names.
+		TSTagDelimiter = { fg = cp.catppuccin5 }, -- Tag delimiter like < > /
+		TSText = { fg = cp.catppuccin10 }, -- For strings considerated text in a markup language.
 		-- TSEmphasis          = { };    -- For text to be represented with emphasis.
 		-- TSUnderline         = { };    -- For text to be represented with an underline.
 		-- TSStrike            = { };    -- For strikethrough texcp.
 		-- TSTitle             = { };    -- Text that is part of a title.
 		-- TSLiteral           = { };    -- Literal texcp.
-
-		-- html
-		TSTagAttribute = { fg = cp.catppuccin3, style = "italic" }, -- Tags like html tag names.
-		TSTag = { fg = cp.catppuccin6 }, -- Tags like html tag names.
-		TSTagDelimiter = { fg = cp.catppuccin7 }, -- Tag delimiter like < > /
-		TSText = { fg = cp.catppuccin10 }, -- For strings considerated text in a markup language.
-
+		-- TSURI               = { };    -- Any URI like a link or email.
+		--
 		-- Markdown tresitter parser support
-		TSURI = { fg = cp.catppuccin2, style = "italic,underline" }, -- urls, links and emails
+		TSURI = { fg = cp.catppuccin2, style = "italic" }, -- urls
 		TSLiteral = { fg = cp.catppuccin0, style = "italic" }, -- used for inline code in markdown and for doc in python (""")
 		TSTextReference = { fg = cp.catppuccin7, style = "bold" }, -- references
 		TSTitle = { fg = cp.catppuccin9, style = "bold" }, -- titles like: # Example
@@ -86,6 +85,9 @@ function M.get(cp)
 		-- bash
 		bashTSFuncBuiltin = { fg = cp.catppuccin5, style = "italic" },
 		bashTSParameter = { fg = cp.catppuccin8, style = "italic" },
+
+		-- lua
+		luaTSField = { fg = cp.catppuccin8 },
 	}
 end
 
