4b8765e0e77f998b7fc6efae1a19bfd8eb1b672d
Pocco81 <pocco451@gmail.com>
2022-01-06 15:22:15 -0500

    updated black1 and removed dashes from palette

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,26 +1,26 @@
 local color_palette = {
 	rosewater = "#F5E0DC", -- Rosewater
 	flamingo = "#F2CDCD", -- Flamingo
-	mauve = "#DEB3EE", -- Mauve -
+	mauve = "#DEB3EE", -- Mauve
 	pink = "#F5C2E7", -- Pink
 	red = "#F28FAD", -- Red
 	maroon = "#E8A2AF", -- Maroon
 	peach = "#F8BD96", -- Peach
-	yellow = "#FAE3B0", -- Yellow -
-	green = "#ACE6AD", -- Green -
-	blue = "#96CDFB", -- Blue -
-	sky = "#89DCEB", -- Sky -
+	yellow = "#FAE3B0", -- Yellow
+	green = "#ACE6AD", -- Green
+	blue = "#96CDFB", -- Blue
+	sky = "#89DCEB", -- Sky
 	teal = "#B5E8E0", -- Teal
 	lavender = "#C9CBFF", -- Lavender
-	white = "#D9E0EE", -- White -
+	white = "#D9E0EE", -- White
 	gray2 = "#C3BAC6", -- Gray2
 	gray1 = "#988BA2", -- Gray1
 	gray0 = "#6E6C7E", -- Gray0
 	black4 = "#575268", -- Black4
-	black3 = "#302D41", -- Black3 -
-	black2 = "#1E1D2F", -- Black2 -
-	black1 = "#1B1923", -- Black1
-	black0 = "#131020", -- Black0 -
+	black3 = "#302D41", -- Black3
+	black2 = "#1E1D2F", -- Black2
+	black1 = "#1A1823", -- Black1
+	black0 = "#131020", -- Black0
 }
 
 return color_palette
