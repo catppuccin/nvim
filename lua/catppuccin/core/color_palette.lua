2b30a4a9972eb3473e6a4dc550046a00e94e9f54
Pocco81 <pocco451@gmail.com>
2022-01-06 11:52:23 -0500

    update - looking good

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,26 +1,26 @@
 local color_palette = {
 	rosewater = "#F5E0DC", -- Rosewater
 	flamingo = "#F2CDCD", -- Flamingo
-	mauve = "#DFB1EB", -- Mauve
+	mauve = "#DEB3EE", -- Mauve -
 	pink = "#F5C2E7", -- Pink
-	red = "#F08FA9", -- Red
+	red = "#F28FAD", -- Red
 	maroon = "#E8A2AF", -- Maroon
-	peach = "#F9C296", -- Peach
-	yellow = "#F0DEAA", -- Yellow
-	green = "#ACE5AB", -- Green
-	blue = "#98BBF5", -- Blue
-	sky = "#89DCEB", -- Sky
+	peach = "#F8BD96", -- Peach
+	yellow = "#FAE3B0", -- Yellow -
+	green = "#ACE6AD", -- Green -
+	blue = "#96CDFB", -- Blue -
+	sky = "#89DCEB", -- Sky -
 	teal = "#B5E8E0", -- Teal
 	lavender = "#C9CBFF", -- Lavender
-	white = "#DEDDF0", -- White
+	white = "#D9E0EE", -- White -
 	gray2 = "#C3BAC6", -- Gray2
 	gray1 = "#988BA2", -- Gray1
 	gray0 = "#6E6C7E", -- Gray0
 	black4 = "#575268", -- Black4
-	black3 = "#332E41", -- Black3
-	black2 = "#1E1D2D", -- Black2
+	black3 = "#302D41", -- Black3 -
+	black2 = "#1E1D2F", -- Black2 -
 	black1 = "#1B1923", -- Black1
-	black0 = "#15121C", -- Black0
+	black0 = "#131020", -- Black0 -
 }
 
 return color_palette
