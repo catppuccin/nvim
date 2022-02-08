406fdf2f2d2372df52d503e9f7bef96d89901c9f
Pocco81 <pocco451@gmail.com>
2022-02-07 22:14:43 -0500

    feat #102: better markdown support

--- lua/catppuccin/core/integrations/markdown.lua
+++ lua/catppuccin/core/integrations/markdown.lua
@@ -5,9 +5,13 @@ function M.get(cp)
 		markdownHeadingDelimiter = { fg = cp.peach, style = "bold" },
 		markdownCode = { fg = cp.flamingo },
 		markdownCodeBlock = { fg = cp.flamingo },
-		markdownH1 = { fg = cp.pink, style = "bold" },
-		markdownH2 = { fg = cp.blue, style = "bold" },
 		markdownLinkText = { fg = cp.blue, style = "underline" },
+		markdownH1 = { fg = cp.lavender },
+		markdownH2 = { fg = cp.mauve },
+		markdownH3 = { fg = cp.green },
+		markdownH4 = { fg = cp.yellow },
+		markdownH5 = { fg = cp.pink },
+		markdownH6 = { fg = cp.teal },
 	}
 end
 
