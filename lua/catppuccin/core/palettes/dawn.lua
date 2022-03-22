4c3c3832f216d8e2472c7ac09f13b933a72a9bd7
Pocco81 <pocco451@gmail.com>
2022-03-21 22:02:07 -0500

    feat: added dawn palette

---
+++ lua/catppuccin/core/palettes/dawn.lua
@@ -0,0 +1,28 @@
+local color_palette = {
+	rosewater = "#E09D90", -- Rosewater
+	flamingo = "#E79C9C", -- Flamingo
+	mauve = "#AC61DF", -- Mauve				-- done
+	pink = "#EF9AD9", -- Pink				-- done
+	red = "#E84746", -- Red					-- done -
+	maroon = "#c96765", -- Maroon			-- done
+	peach = "#E99726", -- Peach				-- done -
+	yellow = "#EABE24", -- Yellow			-- done -
+	green = "#89B517", -- Green				-- done -
+	blue = "#4877D7", -- Blue				-- done -
+	sky = "#3FC4D9", -- Sky					-- done
+	teal = "#149C9B", -- Teal				-- done -
+	lavender = "#939AFB", -- Lavender		-- done
+	white = "#343b58", -- White				-- works?
+	gray2 = "#424968", -- Gray2
+	gray1 = "#505677", -- Gray1
+	gray0 = "#606689", -- Gray0				-- done -
+	black4 = "#cfd8dc", -- Black4
+	black3 = "#eff3f4", -- Black3
+	black2 = "#FCFCFC", -- Black2			-- done
+	black1 = "#F3F3F3", -- Black1
+	black0 = "#D3D0D2", -- Black0
+}
+
+-- NOTE: we might have to remake our current naming convention for gray# and black#
+
+return color_palette
