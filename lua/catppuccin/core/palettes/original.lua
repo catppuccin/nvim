4a3c8f886e596be14d435c48a3f3e6a348c71b2f
ZenithDS <sharmadanshu01@gmail.com>
2022-05-26 12:08:18 -0400

    added some of the older colors

---
+++ lua/catppuccin/core/palettes/original.lua
@@ -0,0 +1,34 @@
+-- NOTE: This is just the mocha color_palette changed to have "old" colors
+
+local color_palette = {
+	rosewater = "#F5E0DC",
+	flamingo = "#F2CDCD",
+	pink = "#F5C2E7",
+	mauve = "#DDB6F2",
+	red = "#F28FAD",
+	maroon = "#E8A2AF",
+	peach = "#F8BD96",
+	yellow = "#FAE3B0",
+	green = "#ABE9B3",
+	teal = "#B5E8E0",
+	sky = "#89DCEB",
+	sapphire = "#74C7EC",
+	blue = "#96CDFB",
+	lavender = "#C9CBFF",
+
+	text = "#D9E0EE",
+	subtext1 = "#B3BCDF",
+	subtext0 = "#A1A8C9",
+	overlay2 = "#8E95B3",
+	overlay1 = "#7B819D",
+	overlay0 = "#696D86",
+	surface2 = "#565970",
+	surface1 = "#43465A",
+	surface0 = "#313244",
+
+	base = "#1E1E2E",
+	mantle = "#181825",
+	crust = "#11111B",
+}
+
+return color_palette
