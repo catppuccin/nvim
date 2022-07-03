fb2a69c7fff1fa9afe4d101454802ca026ed16dd
anon <anon@localhost>
2022-07-03 15:29:53 +0300

    support overriding colors using vim.g.catppuccin_override_colors

--- lua/catppuccin/core/palettes/init.lua
+++ lua/catppuccin/core/palettes/init.lua
@@ -3,10 +3,20 @@ local M = {}
 function M.get_palette()
 	local flvr = vim.g.catppuccin_flavour
 
+	local palette = require("catppuccin.core.palettes.mocha")
 	if flvr == "mocha" or flvr == "latte" or flvr == "macchiato" or flvr == "frappe" then
-		return require("catppuccin.core.palettes." .. flvr)
+		palette = require("catppuccin.core.palettes." .. flvr)
 	end
-	return require("catppuccin.core.palettes.mocha")
+
+	if type(vim.g.catppuccin_override_colors) == "table" then
+		for k, v in pairs(vim.g.catppuccin_override_colors) do
+			if palette[k] then
+				palette[k] = v
+			end
+		end
+	end
+
+	return palette
 end
 
 return M
