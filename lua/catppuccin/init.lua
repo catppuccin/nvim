690c7ccc955023eb9704952e212de9731ef1a669
nullchilly <nullchilly@gmail.com>
2022-07-14 04:01:46 +0700

    fix(command): remove main.lua

--- lua/catppuccin/init.lua
+++ lua/catppuccin/init.lua
@@ -1,13 +1,5 @@
 local M = {}
 
-function M.load()
-	require("catppuccin.main").main("load")
-end
-
-function M.setup(custom_opts)
-	require("catppuccin.config").set_options(custom_opts)
-end
-
 local flavours = {"latte", "frappe", "macchiato", "mocha"}
 
 local command = vim.api.nvim_create_user_command
@@ -36,5 +28,44 @@ end, {})
 command("CatppuccinClean", function()
 	require("catppuccin.utils.util").clean()
 end, {})
+
+function M.load()
+	local catppuccin = require("catppuccin")
+
+	if catppuccin.before_loading ~= nil then
+		catppuccin.before_loading()
+	end
+
+	local loaded = nil
+	local config = require("catppuccin.config").options
+
+	if config.compile.enable == true then
+		local compiled_path = config.compile.path
+			.. (vim.loop.os_uname().sysname == "Windows" and "\\" or "/")
+			.. vim.g.catppuccin_flavour .. config.compile.suffix .. ".lua"
+		local f = io.open(compiled_path, "r")
+		if f ~= nil then
+			io.close(f)
+			vim.cmd("luafile " .. compiled_path)
+			loaded = true
+		end
+	end
+
+	if not loaded then -- colorscheme gets evaluated from mapper.lua
+		local theme = require("catppuccin.core.mapper").apply()
+		local utils = require("catppuccin.utils.util")
+		utils.load(theme)
+
+		if catppuccin.after_loading ~= nil then
+			catppuccin.after_loading()
+		end
+	end
+
+	vim.api.nvim_exec_autocmds("User", { pattern = "CatppuccinLoaded" })
+end
+
+function M.setup(custom_opts)
+	require("catppuccin.config").set_options(custom_opts)
+end
 
 return M
