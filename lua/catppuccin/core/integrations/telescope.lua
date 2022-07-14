408fdbf98ec90ec2c43f0bf218acb3b5e7f17b92
nullchilly <nullchilly@gmail.com>
2022-07-15 00:15:09 +0700

    ci(github): Revert test

--- lua/catppuccin/core/integrations/telescope.lua
+++ lua/catppuccin/core/integrations/telescope.lua
@@ -7,16 +7,16 @@ function M.get(cp)
 		TelescopeSelection = { fg = cp.text, bg = cp.surface0, style = "bold" },
 		TelescopeMatching = { fg = cp.blue },
 		-- Allow Telescope to hijack these highlights in order to achieve the old behavior
-		-- TelescopePromptPrefix = { bg = cp.crust },
-		-- TelescopePromptNormal = { bg = cp.crust},
-		-- TelescopeResultsNormal = { bg = cp.mantle},
-		-- TelescopePreviewNormal = { bg = cp.crust },
-		-- TelescopePromptBorder = { bg = cp.crust, fg = cp.crust },
-		-- TelescopeResultsBorder = { bg = cp.mantle, fg = cp.crust },
-		-- TelescopePreviewBorder = { bg = cp.crust, fg = cp.crust },
-		-- TelescopePromptTitle = { fg = cp.crust },
-		-- TelescopeResultsTitle = { fg = cp.text },
-		-- TelescopePreviewTitle = { fg = cp.crust },
+		--   TelescopePromptPrefix = { bg = cp.crust },
+		--   TelescopePromptNormal = { bg = cp.crust},
+		--   TelescopeResultsNormal = { bg = cp.mantle},
+		--   TelescopePreviewNormal = { bg = cp.crust },
+		--   TelescopePromptBorder = { bg = cp.crust, fg = cp.crust },
+		--   TelescopeResultsBorder = { bg = cp.mantle, fg = cp.crust },
+		--   TelescopePreviewBorder = { bg = cp.crust, fg = cp.crust },
+		--   TelescopePromptTitle = { fg = cp.crust },
+		--   TelescopeResultsTitle = { fg = cp.text },
+		--   TelescopePreviewTitle = { fg = cp.crust },
 	}
 end
 
