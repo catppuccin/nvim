14f3e3841f9bdbb4a24aea4541ba8764b791a9b5
Pocco81 <pocco451@gmail.com>
2022-07-18 19:38:20 -0500

    moved command creation stuff under plugin/

--- lua/catppuccin/init.lua
+++ lua/catppuccin/init.lua
@@ -27,35 +27,4 @@ function M.setup(custom_opts)
 	require("catppuccin.config").set_options(custom_opts)
 end
 
-function M.make_commands()
-	local flavours = { "latte", "frappe", "macchiato", "mocha" }
-	local command = vim.api.nvim_create_user_command
-
-	command("Catppuccin", function(inp)
-		if not vim.tbl_contains(flavours, inp.args) then
-			require("catppuccin.utils.echo")("Invalid flavour")
-			return
-		end
-		vim.g.catppuccin_flavour = inp.args
-		vim.cmd("colorscheme catppuccin")
-	end, {
-		nargs = 1,
-		complete = function(line)
-			return vim.tbl_filter(function(val)
-				return vim.startswith(val, line)
-			end, flavours)
-		end,
-	})
-
-	if require("catppuccin.config").options.compile.enabled == true then
-		command("CatppuccinCompile", function()
-			require("catppuccin.lib.compiler").compile()
-		end, {})
-
-		command("CatppuccinClean", function()
-			require("catppuccin.lib.compiler").clean()
-		end, {})
-	end
-end
-
 return M
--- plugin/catppuccin.lua
+++ plugin/catppuccin.lua
@@ -1,7 +1,33 @@
 if vim.g.loaded_catppuccin then
-  return
+	return
 end
 vim.g.loaded_catppuccin = true
 
--- setup modules
-require("catppuccin").make_commands()
+local flavours = { "latte", "frappe", "macchiato", "mocha" }
+local command = vim.api.nvim_create_user_command
+
+command("Catppuccin", function(inp)
+	if not vim.tbl_contains(flavours, inp.args) then
+		require("catppuccin.utils.echo")("Invalid flavour")
+		return
+	end
+	vim.g.catppuccin_flavour = inp.args
+	vim.cmd("colorscheme catppuccin")
+end, {
+	nargs = 1,
+	complete = function(line)
+		return vim.tbl_filter(function(val)
+			return vim.startswith(val, line)
+		end, flavours)
+	end,
+})
+
+if require("catppuccin.config").options.compile.enabled == true then
+	command("CatppuccinCompile", function()
+		require("catppuccin.lib.compiler").compile()
+	end, {})
+
+	command("CatppuccinClean", function()
+		require("catppuccin.lib.compiler").clean()
+	end, {})
+end
