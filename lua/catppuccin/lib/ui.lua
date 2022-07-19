a7571f1ae932a91ecc282d7f1da50a9f9312d63a
Pocco81 <pocco451@gmail.com>
2022-07-19 12:32:46 -0500

    fix: dim

--- lua/catppuccin/lib/ui.lua
+++ lua/catppuccin/lib/ui.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-local cp = require("catppuccin.palettes.init").get_palette()
 local cnf = require("catppuccin.config").options
 local dim_percentage = cnf.dim_inactive.percentage
 local ucolors = require("catppuccin.utils.colors")
-local latte = require("catppuccin.palettes.latte")
 
 function M.dim()
+	local cp = require("catppuccin.palettes.init").get_palette()
 	if cnf.dim_inactive.shade == "dark" then
 		return ucolors.vary_color(
-			{ latte = ucolors.darken(latte.base, dim_percentage, latte.mantle) },
+			{ latte = ucolors.darken(cp.base, dim_percentage, cp.mantle) },
 			ucolors.darken(cp.base, dim_percentage, cp.mantle)
 		)
 	end
 	return ucolors.vary_color(
-		{ latte = ucolors.lighten("#FBFCFD", dim_percentage, latte.base) },
+		{ latte = ucolors.lighten("#FBFCFD", dim_percentage, cp.base) },
 		ucolors.lighten(cp.surface0, dim_percentage, cp.base)
 	)
 end
