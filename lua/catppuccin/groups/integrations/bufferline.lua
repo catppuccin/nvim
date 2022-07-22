5f65938b643f001214d30c0001771c5519b8d514
nullchilly <nullchilly@gmail.com>
2022-07-22 13:59:50 +0700

    fix(integration): Bufferline slant style

--- lua/catppuccin/groups/integrations/bufferline.lua
+++ lua/catppuccin/groups/integrations/bufferline.lua
@@ -6,27 +6,31 @@ function M.get()
 	local bg_highlight = (transparent_background and cnf.dim_inactive.enabled and cp.dim)
 		or (transparent_background and "NONE")
 		or (cnf.dim_inactive.enabled and cp.dim)
-		or cp.base
+		or cp.crust
 
 	local inactive_bg = transparent_background and "NONE" or cp.mantle
 
 	return {
-		BufferLineFill = { bg = bg_highlight },
+		-- buffers
+		BufferLineBackground = { bg = cp.mantle },
 		BufferLineBackcrust = { fg = cp.text, bg = inactive_bg }, -- others
 		BufferLineBufferVisible = { fg = cp.surface1, bg = inactive_bg },
 		BufferLineBufferSelected = { fg = cp.text, bg = cp.base, style = { "bold", "italic" } }, -- current
-		BufferLineTab = { fg = cp.surface1, bg = cp.base },
-		BufferLineTabSelected = { fg = cp.red, bg = cp.blue },
+		-- tabs
+		BufferLineTab = { fg = cp.surface1, bg = cp.mantle },
+		BufferLineTabSelected = { fg = cp.sky, bg = cp.base },
 		BufferLineTabClose = { fg = cp.red, bg = inactive_bg },
 		BufferLineIndicatorSelected = { fg = cp.peach, bg = cp.base },
 		-- separators
-		BufferLineSeparator = { fg = inactive_bg, bg = inactive_bg },
-		BufferLineSeparatorVisible = { fg = inactive_bg, bg = inactive_bg },
-		BufferLineSeparatorSelected = { fg = inactive_bg, bg = inactive_bg },
+		BufferLineSeparator = { fg = cp.crust, bg = inactive_bg },
+		BufferLineSeparatorVisible = { fg = cp.crust, bg = inactive_bg },
+		BufferLineSeparatorSelected = { fg = cp.crust, bg = cp.base },
 		-- close buttons
 		BufferLineCloseButton = { fg = cp.surface1, bg = inactive_bg },
 		BufferLineCloseButtonVisible = { fg = cp.surface1, bg = inactive_bg },
 		BufferLineCloseButtonSelected = { fg = cp.red, bg = cp.base },
+		-- Empty fill
+		BufferLineFill = { bg = bg_highlight },
 	}
 end
 
