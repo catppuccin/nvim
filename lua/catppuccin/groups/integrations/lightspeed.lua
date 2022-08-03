d61cad6a8330fa585fe0a7acc9b4d1059db8da95
nullchilly <nullchilly@gmail.com>
2022-08-04 00:43:59 +0700

    integration(lightspeed): better shortcut for latte flavour

--- lua/catppuccin/groups/integrations/lightspeed.lua
+++ lua/catppuccin/groups/integrations/lightspeed.lua
@@ -7,7 +7,7 @@ function M.get()
 		LightspeedLabel = { fg = cp.red, style = { "bold", "underline" } },
 		LightspeedLabelDistant = { fg = cp.sky },
 		LightspeedShortcut = {
-			fg = ucolors.brighten(cp.text, 0.7),
+			fg = "#ffffff",
 			bg = ucolors.increase_saturation(cp.red, 0.7),
 			style = { "bold" },
 		},
