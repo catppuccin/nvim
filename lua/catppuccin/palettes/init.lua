624b427c8751d2cbbc8c3bd789fa8dfc408e4be5
nullchilly <nullchilly@gmail.com>
2022-08-03 23:23:07 +0700

    Clearer flavour error messages Closes #223

--- lua/catppuccin/palettes/init.lua
+++ lua/catppuccin/palettes/init.lua
@@ -4,7 +4,7 @@ function M.get_palette(flavour)
 	local flvr = flavour or vim.g.catppuccin_flavour or "macchiato"
 	if not vim.tbl_contains({ "latte", "frappe", "macchiato", "mocha" }, flvr) then
 		require("catppuccin.utils.echo")(
-			"Invalid flavour, g:catppuccin_flavour must be latte, frappe, macchiato or mocha",
+			"Invalid flavour, vim.g.catppuccin_flavour (lua) or g:catppuccin_flavour (vimscript) must be 'latte', 'frappe', 'macchiato' or 'mocha'. Check your spelling.",
 			"error"
 		)
 	end
