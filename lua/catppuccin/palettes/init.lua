24b5458e11f2984872680ea13ab3117d98756565
nullchilly <nullchilly@gmail.com>
2022-08-03 12:18:54 +0700

    feat: Throw error for invalid flavour Closes #222

--- lua/catppuccin/palettes/init.lua
+++ lua/catppuccin/palettes/init.lua
@@ -2,6 +2,12 @@ local M = {}
 
 function M.get_palette(flavour)
 	local flvr = flavour or vim.g.catppuccin_flavour or "macchiato"
+	if not vim.tbl_contains({ "latte", "frappe", "macchiato", "mocha" }, vim.g.catppuccin_flavour) then
+		require("catppuccin.utils.echo")(
+			"Invalid flavour, g:catppuccin_flavour must be latte, frappe, macchiato or mocha",
+			"error"
+		)
+	end
 	local _, palette = pcall(require, "catppuccin.palettes." .. flvr)
 	local cnf = require("catppuccin.config").options
 	return vim.tbl_deep_extend("keep", cnf.color_overrides.all or {}, cnf.color_overrides[flvr] or {}, palette or {})
