53ad24e78f4d2185972e3ae68025647f2e449b18
nullchilly <nullchilly@gmail.com>
2022-08-04 00:36:40 +0700

    improve lightspeed highlights

--- lua/catppuccin/groups/integrations/lightspeed.lua
+++ lua/catppuccin/groups/integrations/lightspeed.lua
@@ -1,10 +1,16 @@
 local M = {}
 
+local ucolors = require("catppuccin.utils.colors")
+
 function M.get()
 	return {
 		LightspeedLabel = { fg = cp.red, style = { "bold", "underline" } },
 		LightspeedLabelDistant = { fg = cp.sky },
-		LightspeedShortcut = { fg = cp.text, bg = "#ff2f87", style = { "bold" } },
+		LightspeedShortcut = {
+			fg = ucolors.brighten(cp.text, 0.7),
+			bg = ucolors.increase_saturation(cp.red, 0.7),
+			style = { "bold" },
+		},
 		LightspeedMaskedChar = { fg = cp.red },
 		LightspeedUnlabeledMatch = { fg = cp.text, style = { "bold" } },
 		LightspeedGreyWash = { link = "Comment" },
