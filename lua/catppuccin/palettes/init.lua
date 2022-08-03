184e5ce9316e2ae88fa3ba5cba72c5db790a6b23
nullchilly <nullchilly@gmail.com>
2022-08-03 23:39:02 +0700

    Remove get_palette warnings

--- lua/catppuccin/palettes/init.lua
+++ lua/catppuccin/palettes/init.lua
@@ -1,14 +1,7 @@
 local M = {}
 
 function M.get_palette(flavour)
-	local flvr = flavour or vim.g.catppuccin_flavour
-	if flvr == nil then
-		flvr = "macchiato"
-		require("catppuccin.utils.echo")(
-			"g:catppuccin_flavour must be set before calling require('catppuccin.palettes').get_palette(). Defaulting to macchiato flavour",
-			"info"
-		)
-	end
+	local flvr = flavour or vim.g.catppuccin_flavour or "macchiato"
 	if not vim.tbl_contains({ "latte", "frappe", "macchiato", "mocha" }, flvr) then
 		require("catppuccin.utils.echo")(
 			"Invalid flavour, vim.g.catppuccin_flavour (lua) or g:catppuccin_flavour (vimscript) must be 'latte', 'frappe', 'macchiato' or 'mocha'. Check your spelling.",
