86dd202ad0cfeba6b1a3b3f05bd1311aa193e57d
nullchilly <nullchilly@gmail.com>
2022-08-04 18:10:37 +0700

    fix(integration): leap.nvim for latte flavour

--- lua/catppuccin/groups/integrations/leap.lua
+++ lua/catppuccin/groups/integrations/leap.lua
@@ -4,13 +4,20 @@ local ucolors = require("catppuccin.utils.colors")
 
 function M.get()
 	return {
-		LeapMatch = { fg = ucolors.brighten(cp.green, 0.3), style = { "underline", "nocombine" } },
+		LeapMatch = {
+			fg = ucolors.vary_color({ latte = "#222222" }, ucolors.brighten(cp.green, 0.3)),
+			style = { "underline", "nocombine" },
+		},
 		LeapLabelPrimary = {
-			fg = cp.base,
-			bg = ucolors.brighten(cp.green, 0.3),
+			fg = ucolors.vary_color({ latte = "#222222" }, cp.base),
+			bg = ucolors.vary_color({ latte = ucolors.brighten(cp.red, 0.4) }, ucolors.brighten(cp.green, 0.3)),
 			style = { "nocombine" },
 		},
-		LeapLabelSecondary = { fg = cp.base, bg = cp.sky, style = { "nocombine" } },
+		LeapLabelSecondary = {
+			fg = ucolors.vary_color({ latte = "#222222" }, cp.base),
+			bg = cp.sky,
+			style = { "nocombine" },
+		},
 	}
 end
 
