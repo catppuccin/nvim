eb526ac63ad1671a4fff9b1b6e10696f0725ade0
Michael Lan <michaellan202@gmail.com>
2022-08-06 17:50:42 -0700

    fix(neogit): improve Neogit{Hunk,Diff} colors

--- lua/catppuccin/groups/integrations/neogit.lua
+++ lua/catppuccin/groups/integrations/neogit.lua
@@ -1,14 +1,30 @@
 local M = {}
+local ucolors = require("catppuccin.utils.colors")
 
 function M.get()
 	return {
 		NeogitBranch = { fg = cp.pink },
 		NeogitRemote = { fg = cp.pink },
-		NeogitHunkHeader = { bg = cp.blue, fg = cp.text },
-		NeogitHunkHeaderHighlight = { bg = cp.base, fg = cp.blue },
-		NeogitDiffContextHighlight = { bg = cp.mantle, fg = cp.overlay2 },
-		NeogitDiffDeleteHighlight = { bg = cp.mantle, fg = cp.red },
-		NeogitDiffAddHighlight = { bg = cp.mantle, fg = cp.green },
+		NeogitHunkHeader = {
+			bg = ucolors.darken(cp.blue, 0.095, cp.base),
+			fg = ucolors.darken(cp.blue, 0.5, cp.base),
+		},
+		NeogitHunkHeaderHighlight = {
+			bg = ucolors.darken(cp.blue, 0.215, cp.base),
+			fg = cp.blue
+		},
+		NeogitDiffContextHighlight = {
+			bg = ucolors.darken(cp.blue, 0.065, cp.base),
+			fg = cp.text
+		},
+		NeogitDiffDeleteHighlight = {
+			bg = ucolors.darken(cp.red, 0.095, cp.base),
+			fg = cp.red
+		},
+		NeogitDiffAddHighlight = {
+			bg = ucolors.darken(cp.green, 0.095, cp.base),
+			fg = cp.green
+		}
 	}
 end
 
