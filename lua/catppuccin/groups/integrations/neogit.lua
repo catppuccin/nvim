9d7456da6e4018b82d85633e9bbe759d83f36a19
Michael Lan <michaellan202@gmail.com>
2022-08-06 20:46:52 -0700

    formatting

--- lua/catppuccin/groups/integrations/neogit.lua
+++ lua/catppuccin/groups/integrations/neogit.lua
@@ -25,18 +25,18 @@ function M.get()
 			bg = ucolors.darken(cp.green, 0.095, cp.base),
 			fg = cp.green,
 		},
-    NeogitCommitViewHeader = {
-      bg = ucolors.darken(cp.blue, 0.300, cp.base),
-      fg = ucolors.lighten(cp.blue, 0.800, cp.text),
-    },
-    NeogitDiffDelete = {
+		NeogitCommitViewHeader = {
+			bg = ucolors.darken(cp.blue, 0.300, cp.base),
+			fg = ucolors.lighten(cp.blue, 0.800, cp.text),
+		},
+		NeogitDiffDelete = {
 			bg = ucolors.darken(cp.red, 0.125, cp.base),
 			fg = ucolors.darken(cp.red, 0.445, cp.base),
-    },
-    NeogitDiffAdd = {
+		},
+		NeogitDiffAdd = {
 			bg = ucolors.darken(cp.green, 0.125, cp.base),
 			fg = ucolors.darken(cp.green, 0.445, cp.base),
-    },
+		},
 	}
 end
 
