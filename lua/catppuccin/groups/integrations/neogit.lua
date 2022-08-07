44a4cb89b54a6252a9c48d0c182086ace51f0977
Michael Lan <michaellan202@gmail.com>
2022-08-06 21:25:56 -0700

    fix: make inactive more legible

--- lua/catppuccin/groups/integrations/neogit.lua
+++ lua/catppuccin/groups/integrations/neogit.lua
@@ -18,24 +18,24 @@ function M.get()
 			fg = cp.text,
 		},
 		NeogitDiffDeleteHighlight = {
-			bg = ucolors.darken(cp.red, 0.095, cp.base),
-			fg = cp.red,
+			bg = ucolors.darken(cp.red, 0.345, cp.base),
+			fg = ucolors.lighten(cp.red, 0.850, cp.text),
 		},
 		NeogitDiffAddHighlight = {
+			bg = ucolors.darken(cp.green, 0.345, cp.base),
+			fg = ucolors.lighten(cp.green, 0.850, cp.text),
+		},
+		NeogitDiffDelete = {
+			bg = ucolors.darken(cp.red, 0.095, cp.base),
+			fg = ucolors.darken(cp.red, 0.800, cp.base),
+		},
+		NeogitDiffAdd = {
 			bg = ucolors.darken(cp.green, 0.095, cp.base),
-			fg = cp.green,
+			fg = ucolors.darken(cp.green, 0.800, cp.base),
 		},
 		NeogitCommitViewHeader = {
 			bg = ucolors.darken(cp.blue, 0.300, cp.base),
 			fg = ucolors.lighten(cp.blue, 0.800, cp.text),
-		},
-		NeogitDiffDelete = {
-			bg = ucolors.darken(cp.red, 0.125, cp.base),
-			fg = ucolors.darken(cp.red, 0.445, cp.base),
-		},
-		NeogitDiffAdd = {
-			bg = ucolors.darken(cp.green, 0.125, cp.base),
-			fg = ucolors.darken(cp.green, 0.445, cp.base),
 		},
 	}
 end
