b218d4b9d6ec1627b532fb76c2a91a8914c12fad
MikiS <73443709+RAV64@users.noreply.github.com>
2022-08-29 10:04:35 +0300

    Fix: TS-Rainbow

--- lua/catppuccin/groups/integrations/ts_rainbow.lua
+++ lua/catppuccin/groups/integrations/ts_rainbow.lua
@@ -1,20 +1,14 @@
 local M = {}
 
 function M.get()
-	local cnf = require("catppuccin.config").options
-	local transparent_background = cnf.transparent_background
-	local bg_highlight = (transparent_background and cnf.dim_inactive.enabled and cp.dim)
-		or (transparent_background and "NONE")
-		or (cnf.dim_inactive.enabled and cp.dim)
-		or cp.base
 	return {
-		rainbowcol1 = { bg = bg_highlight, fg = cp.red },
-		rainbowcol2 = { bg = bg_highlight, fg = cp.teal },
-		rainbowcol3 = { bg = bg_highlight, fg = cp.yellow },
-		rainbowcol4 = { bg = bg_highlight, fg = cp.blue },
-		rainbowcol5 = { bg = bg_highlight, fg = cp.pink },
-		rainbowcol6 = { bg = bg_highlight, fg = cp.flamingo },
-		rainbowcol7 = { bg = bg_highlight, fg = cp.green },
+		rainbowcol1 = {fg = cp.red },
+		rainbowcol2 = {fg = cp.teal },
+		rainbowcol3 = {fg = cp.yellow },
+		rainbowcol4 = {fg = cp.blue },
+		rainbowcol5 = {fg = cp.pink },
+		rainbowcol6 = {fg = cp.flamingo },
+		rainbowcol7 = {fg = cp.green },
 	}
 end
 
