37395f17d551dcefb9311c327bcf8ebd2a3e910b
Jae-Heon Ji <atx6419@gmail.com>
2022-09-06 16:27:45 +0900

    chore: delete duplicated highlight group

--- lua/catppuccin/groups/integrations/which_key.lua
+++ lua/catppuccin/groups/integrations/which_key.lua
@@ -6,7 +6,6 @@ function M.get()
 		WhichKeyGroup = { fg = cp.blue },
 		WhichKeyDesc = { fg = cp.pink },
 		WhichKeySeperator = { fg = cp.overlay0 },
-		WhichKeySeparator = { fg = cp.overlay0 },
 		WhichKeyFloat = { bg = cp.crust },
 		WhichKeyValue = { fg = cp.overlay0 },
 	}
