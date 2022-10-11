2ea154d045ce93f16fa2a998965ea380b92d8f09
Myles Mo <54089360+emxxjnm@users.noreply.github.com>
2022-10-11 15:14:08 +0800

    fix: respect transparency (#300)
    * fix(hop): respect transparency
    
    * fix(which-key): respect transparency

--- lua/catppuccin/groups/integrations/hop.lua
+++ lua/catppuccin/groups/integrations/hop.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.get()
+	local bg = cnf.transparent_background and cp.none or cp.base
 	return {
-		HopNextKey = { bg = cp.base, fg = cp.peach, style = { "bold", "underline" } },
-		HopNextKey1 = { bg = cp.base, fg = cp.blue, style = { "bold" } },
-		HopNextKey2 = { bg = cp.base, fg = cp.teal, style = { "bold", "italic" } },
-		HopUnmatched = { bg = cp.base, fg = cp.overlay0 },
+		HopNextKey = { bg = bg, fg = cp.peach, style = { "bold", "underline" } },
+		HopNextKey1 = { bg = bg, fg = cp.blue, style = { "bold" } },
+		HopNextKey2 = { bg = bg, fg = cp.teal, style = { "bold", "italic" } },
+		HopUnmatched = { bg = bg, fg = cp.overlay0 },
 	}
 end
 
