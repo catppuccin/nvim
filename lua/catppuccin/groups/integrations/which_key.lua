23b1dd00f447ab38d66ef5bb1e37242258c06e28
emxxjnm <emxxjnm@example>
2022-10-11 15:04:33 +0800

    fix(which-key): respect transparency

--- lua/catppuccin/groups/integrations/which_key.lua
+++ lua/catppuccin/groups/integrations/which_key.lua
@@ -6,7 +6,7 @@ function M.get()
 		WhichKeyGroup = { fg = cp.blue },
 		WhichKeySeperator = { fg = cp.overlay0 },
 		WhichKeyDesc = { fg = cp.pink },
-		WhichKeyFloat = { bg = cp.crust },
+		WhichKeyFloat = { bg = cnf.transparent_background and cp.none or cp.crust },
 		WhichKeyBorder = { fg = cp.blue },
 		WhichKeyValue = { fg = cp.overlay0 },
 	}
