2f416e0387062288cd0e8ba8db8f05cc927dad5e
emxxjnm <emxxjnm@example>
2022-10-20 10:53:33 +0800

    fix(gitsigns): preview_hunk respect transparency

--- lua/catppuccin/groups/integrations/gitsigns.lua
+++ lua/catppuccin/groups/integrations/gitsigns.lua
@@ -5,6 +5,9 @@ function M.get()
 		GitSignsAdd = { fg = cp.green }, -- diff mode: Added line |diff.txt|
 		GitSignsChange = { fg = cp.yellow }, -- diff mode: Changed line |diff.txt|
 		GitSignsDelete = { fg = cp.red }, -- diff mode: Deleted line |diff.txt|
+
+		GitSignsAddPreview = cnf.transparent_background and { fg = cp.green, bg = cp.none } or { link = "DiffAdd" },
+		GitSignsDeletePreview = cnf.transparent_background and { fg = cp.red, bg = cp.none } or { link = "DiffDelete" },
 	}
 end
 
