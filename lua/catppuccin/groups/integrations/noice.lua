df628154d6b1a38bd961139854346ac4f502170a
Null Chilly <56817415+nullchilly@users.noreply.github.com>
2022-10-22 06:23:02 +0700

    feat: Add noice.nvim integration (#317)

---
+++ lua/catppuccin/groups/integrations/noice.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+local virtual_text = cnf.integrations.native_lsp.virtual_text
+
+function M.get()
+	return { -- Personally the default integration is already pretty good
+		Cmdline = { fg = cp.text },
+		CmdlineIcon = { fg = cp.sky, style = virtual_text.information },
+		CmdlineIconSearch = { fg = cp.yellow },
+		CmdlinePopup = { fg = cp.text, bg = cnf.transparent_background and cp.none or cp.base },
+		CmdlinePopupBorder = { fg = cp.lavender },
+		CmdlinePopupBorderSearch = { fg = cp.yellow },
+		Confirm = { fg = cp.text, bg = cnf.transparent_background and cp.none or cp.base },
+		ConfirmBorder = { fg = cp.blue },
+		Cursor = { link = "Cursor" },
+		Mini = { fg = cp.text },
+		Popup = { fg = cp.text, bg = cnf.transparent_background and cp.none or cp.mantle },
+		PopupBorder = { link = "FloatBorder" },
+		Popupmenu = { link = "Pmenu" },
+		PopupmenuBorder = { link = "FloatBorder" },
+		PopupmenuMatch = { link = "Special" },
+		PopupmenuSelected = { link = "PmenuSel" },
+		Scrollbar = { link = "PmenuSbar" },
+		ScrollbarThumb = { link = "PmenuThumb" },
+		Split = { fg = cp.text, bg = cnf.transparent_background and cp.none or cp.mantle },
+		SplitBorder = { link = "FloatBorder" },
+		VirtualText = { link = "DiagnosticVirtualTextInfo" },
+	}
+end
+
+return M
