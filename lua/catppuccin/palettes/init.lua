d588e6546a2aac441f77448e02741883c0dab9df
Null Chilly <nullchilly@gmail.com>
2022-10-23 11:02:30 +0700

    fix: Default flavour is not set in get_palette

--- lua/catppuccin/palettes/init.lua
+++ lua/catppuccin/palettes/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.get_palette(flavour)
-	local flvr = flavour or require("catppuccin").flavour or vim.g.catppuccin_flavour
+	local flvr = flavour or require("catppuccin").flavour or vim.g.catppuccin_flavour or "mocha"
 	local _, palette = pcall(require, "catppuccin.palettes." .. flvr)
 	local cnf = require("catppuccin").options
 	return vim.tbl_deep_extend("keep", cnf.color_overrides.all or {}, cnf.color_overrides[flvr] or {}, palette or {})
