9b8aee20e90ea6178e9925f584c6ae1b9c69afda
Null Chilly <56817415+nullchilly@users.noreply.github.com>
2022-11-18 19:32:24 +0700

    chore(vim): remove globals (#342)

--- lua/catppuccin/lib/hashing.lua
+++ lua/catppuccin/lib/hashing.lua
@@ -1,12 +1,16 @@
 local M = {}
+local B = bit or bit32 or {
+	rshift = function(a, b) return a >> b end,
+	bxor = function(a, b) return a ~ b end,
+}
 
 local hash_str = function(str) -- MurmurOAAT_32, https://stackoverflow.com/questions/7666509/hash-function-for-string
 	local hash = 0x12345678
 	local tbl = { string.byte(str, 1, #str) }
 	for i = 1, #tbl do
-		hash = bit.bxor(hash, tbl[i])
+		hash = B.bxor(hash, tbl[i])
 		hash = hash * 0x5bd1e995
-		hash = bit.bxor(hash, bit.rshift(hash, 15))
+		hash = B.bxor(hash, B.rshift(hash, 15))
 	end
 	return hash
 end
@@ -24,7 +28,7 @@ function M.hash(tbl) -- Xor hashing: https://codeforces.com/blog/entry/85900
 	else
 		local hash = 0
 		for k, v in pairs(tbl) do
-			hash = bit.bxor(hash, hash_str(k .. ":" .. M.hash(v)))
+			hash = B.bxor(hash, hash_str(k .. ":" .. M.hash(v)))
 		end
 		return hash
 	end
