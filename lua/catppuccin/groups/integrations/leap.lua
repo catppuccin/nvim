27a9a232a1308e56c48991dcaf4bbfffc917e814
Myles Mo <54089360+emxxjnm@users.noreply.github.com>
2022-11-19 11:11:16 +0800

    fix(leap): respect transparency (#346)

--- lua/catppuccin/groups/integrations/leap.lua
+++ lua/catppuccin/groups/integrations/leap.lua
@@ -3,19 +3,21 @@ local M = {}
 function M.get()
 	return {
 		LeapMatch = {
-			fg = U.vary_color({ latte = "#222222" }, U.brighten(C.green, 0.3)),
-			style = { "underline", "nocombine" },
+			fg = O.transparent_background and C.pink or U.vary_color({ latte = "#222222" }, U.brighten(C.green, 0.3)),
+			style = { "underline", "nocombine", O.transparent_background and "bold" or nil },
 		},
 		LeapLabelPrimary = {
-			fg = U.vary_color({ latte = "#222222" }, C.base),
-			bg = U.vary_color({ latte = U.brighten(C.red, 0.4) }, U.brighten(C.green, 0.3)),
-			style = { "nocombine" },
+			fg = O.transparent_background and C.green or U.vary_color({ latte = "#222222" }, C.base),
+			bg = O.transparent_background and C.none
+				or U.vary_color({ latte = U.brighten(C.red, 0.4) }, U.brighten(C.green, 0.3)),
+			style = { "nocombine", O.transparent_background and "bold" or nil },
 		},
 		LeapLabelSecondary = {
-			fg = U.vary_color({ latte = "#222222" }, C.base),
-			bg = U.vary_color({ latte = U.brighten(C.sky, 0.3) }, C.sky),
-			style = { "nocombine" },
+			fg = O.transparent_background and C.blue or U.vary_color({ latte = "#222222" }, C.base),
+			bg = O.transparent_background and C.none or U.vary_color({ latte = U.brighten(C.sky, 0.3) }, C.sky),
+			style = { "nocombine", O.transparent_background and "bold" or nil },
 		},
+		LeapBackdrop = { fg = O.transparent_background and C.overlay0 or C.none },
 	}
 end
 
