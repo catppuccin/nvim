3ac13e6d67d8da7d277f5e17d71453bdebdf8c8c
Null Chilly <nullchilly@gmail.com>
2022-11-19 10:07:07 +0700

    chore: restore style

--- lua/catppuccin/groups/integrations/leap.lua
+++ lua/catppuccin/groups/integrations/leap.lua
@@ -4,21 +4,20 @@ function M.get()
 	return {
 		LeapMatch = {
 			fg = O.transparent_background and C.pink or U.vary_color({ latte = "#222222" }, U.brighten(C.green, 0.3)),
-			underline = true,
-			nocombine = true,
+			style = { "underline", "nocombine" },
 			bold = O.transparent_background,
 		},
 		LeapLabelPrimary = {
 			fg = O.transparent_background and C.green or U.vary_color({ latte = "#222222" }, C.base),
 			bg = O.transparent_background and C.none
 				or U.vary_color({ latte = U.brighten(C.red, 0.4) }, U.brighten(C.green, 0.3)),
-			nocombine = true,
+			style = { "nocombine" },
 			bold = O.transparent_background,
 		},
 		LeapLabelSecondary = {
 			fg = O.transparent_background and C.blue or U.vary_color({ latte = "#222222" }, C.base),
 			bg = O.transparent_background and C.none or U.vary_color({ latte = U.brighten(C.sky, 0.3) }, C.sky),
-			nocombine = true,
+			style = { "nocombine" },
 			bold = O.transparent_background,
 		},
 		LeapBackdrop = { fg = O.transparent_background and C.overlay0 or C.none },
