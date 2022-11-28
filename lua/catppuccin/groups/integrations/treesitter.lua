6652f63ed0ef3590f75ce6bfcf29fd4439ea3920
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-11-28 13:30:14 +0100

    @type.qualifier pink only for php

--- lua/catppuccin/groups/integrations/treesitter.lua
+++ lua/catppuccin/groups/integrations/treesitter.lua
@@ -69,7 +69,8 @@ If you want to stay on nvim 0.7, either disable the integration or pin catppucci
 		["@type"] = { link = "Type" }, -- For types.
 		["@type.builtin"] = { fg = C.yellow, style = O.styles.properties or "italic" }, -- For builtin types.
 		["@type.definition"] = { link = "@type" }, -- type definitions (e.g. `typedef` in C)
-		["@type.qualifier"] = { fg = C.pink, style = O.styles.properties }, -- type qualifiers (e.g. `const`)
+		["@type.qualifier"] = { link = "@type" }, -- type qualifiers (e.g. `const`)
+		["@type.qualifier.php"] = { fg = C.pink, style = O.styles.properties }, -- type qualifiers (e.g. `const`)
 
 		["@storageclass"] = { link = "StorageClass" }, -- visibility/life-time/etc. modifiers (e.g. `static`)
 		["@attribute"] = { link = "Constant" }, -- attribute annotations (e.g. Python decorators)
