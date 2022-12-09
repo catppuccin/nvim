d55d81eabfeacbfb167e2948790ae94aaba3b149
Null Chilly <nullchilly@gmail.com>
2022-12-09 20:48:44 +0700

    fix(compile): restore user flavour

--- lua/catppuccin/init.lua
+++ lua/catppuccin/init.lua
@@ -68,9 +68,12 @@ local M = {
 }
 
 function M.compile()
+	local _flavour = M.flavour
 	for flavour, _ in pairs(M.flavours) do
+		M.flavour = flavour
 		require("catppuccin.lib." .. (is_vim and "vim." or "") .. "compiler").compile(flavour)
 	end
+	M.flavour = _flavour -- Restore user flavour after compile
 end
 
 local lock = false -- Avoid g:colors_name reloading
@@ -78,8 +81,8 @@ local lock = false -- Avoid g:colors_name reloading
 function M.load(flavour)
 	if lock then return end
 	M.flavour = flavour
-		or (vim.g.colors_name and M.options.background[is_vim and vim.eval "&background" or vim.o.background] or M.flavour)
-		or "mocha"
+			or (vim.g.colors_name and M.options.background[is_vim and vim.eval "&background" or vim.o.background] or M.flavour)
+			or "mocha"
 	local compiled_path = M.options.compile_path .. M.path_sep .. M.flavour .. "_compiled.lua"
 	lock = true
 	local f = loadfile(compiled_path)
@@ -140,8 +143,8 @@ vim.api.nvim_create_user_command("Catppuccin", function(inp)
 	if not M.flavours[inp.args] then
 		vim.notify(
 			"Catppuccin (error): Invalid flavour '"
-				.. inp.args
-				.. "', flavour must be 'latte', 'frappe', 'macchiato' or 'mocha'",
+			.. inp.args
+			.. "', flavour must be 'latte', 'frappe', 'macchiato' or 'mocha'",
 			vim.log.levels.ERROR
 		)
 		return
