3020af75aae098a77737d91ee37c7147c8450d99
Stephen Sherratt <stephen@sherra.tt>
2022-12-28 22:36:49 +1100

    fix(0.9): explicitly convert value to string (#372)
    This fixes an error in neovim v0.9:
    bad argument #4 to 'fmt' (string expected, got boolean)

--- lua/catppuccin/lib/compiler.lua
+++ lua/catppuccin/lib/compiler.lua
@@ -9,7 +9,7 @@ local function inspect(t)
 	local list = {}
 	for k, v in pairs(t) do
 		local q = type(v) == "string" and [["]] or ""
-		table.insert(list, fmt([[%s = %s%s%s]], k, q, v, q))
+		table.insert(list, fmt([[%s = %s%s%s]], k, q, tostring(v), q))
 	end
 	return fmt([[{ %s }]], table.concat(list, ", "))
 end
