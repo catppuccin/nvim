491f0a58ba8901258f84de94d848811a6eaf5b98
nullchilly <nullchilly@gmail.com>
2023-01-29 20:12:01 +0700

    feat(lspsaga): keep backwards compatibility

--- lua/catppuccin/groups/integrations/lsp_saga.lua
+++ lua/catppuccin/groups/integrations/lsp_saga.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+function M.custom_colors()
+	local C = require("catppuccin.palettes").get_palette()
+	return {
+		normal_bg = C.base,
+		title_bg = C.green,
+		red = C.red,
+		magenta = C.maroon,
+		orange = C.peach,
+		yellow = C.yellow,
+		green = C.green,
+		cyan = C.sky,
+		blue = C.blue,
+		purple = C.mauve,
+		white = C.text,
+		black = C.crust,
+	}
+end
+
 function M.custom_kind()
 	local C = require("catppuccin.palettes").get_palette()
 	return {
