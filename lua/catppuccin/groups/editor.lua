238d1e98c2656e2efce3dc64771542150d306a39
nullchilly <nullchilly@gmail.com>
2023-02-12 12:40:32 +0700

    fix(group): Comment out MsgArea

--- lua/catppuccin/groups/editor.lua
+++ lua/catppuccin/groups/editor.lua
@@ -25,7 +25,7 @@ function M.get()
 		CursorLineNr = { fg = C.lavender }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line. highlights the number in numberline.
 		MatchParen = { fg = C.peach, style = { "bold" } }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
 		ModeMsg = { fg = C.text, style = { "bold" } }, -- 'showmode' message (e.g., "-- INSERT -- ")
-		MsgArea = { fg = C.text }, -- Area for messages and cmdline
+		-- MsgArea = { fg = C.text }, -- Area for messages and cmdline, don't set this highlight because of https://github.com/neovim/neovim/issues/17832
 		MsgSeparator = {}, -- Separator for scrolled messages, `msgsep` flag of 'display'
 		MoreMsg = { fg = C.blue }, -- |more-prompt|
 		NonText = { fg = C.overlay0 }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
