839d015ce9b6c9447fd8b40e43a6411ccc87ebf1
nullchilly <nullchilly@gmail.com>
2023-03-13 21:25:10 +0700

    feat(semantic_tokens): link default library to function builtin

--- lua/catppuccin/groups/integrations/semantic_tokens.lua
+++ lua/catppuccin/groups/integrations/semantic_tokens.lua
@@ -9,7 +9,8 @@ function M.get()
 		["@lsp.type.parameter"] = { link = "@parameter" },
 		["@lsp.type.property"] = { link = "@property" },
 		["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
-		["@lsp.typemod.function.defaultLibrary"] = { link = "Special" },
+		["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+		["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
 		["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
 	}
 end
