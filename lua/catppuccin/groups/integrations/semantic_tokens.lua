dda3122d399cd2dbc62d72ebe788a1ae1ffa1ab4
nullchilly <nullchilly@gmail.com>
2023-03-22 18:43:42 +0700

    feat(semantic_tokens): link lsp comments Closes #434

--- lua/catppuccin/groups/integrations/semantic_tokens.lua
+++ lua/catppuccin/groups/integrations/semantic_tokens.lua
@@ -2,15 +2,16 @@ local M = {}
 
 function M.get()
 	return {
+		["@lsp.type.comment"] = { link = "@comment" },
 		["@lsp.type.enum"] = { link = "@type" },
-		["@lsp.type.keyword"] = { link = "@keyword" },
 		["@lsp.type.interface"] = { link = "@interface" },
+		["@lsp.type.keyword"] = { link = "@keyword" },
 		["@lsp.type.namespace"] = { link = "@namespace" },
 		["@lsp.type.parameter"] = { link = "@parameter" },
 		["@lsp.type.property"] = { link = "@property" },
 		["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
-		["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
 		["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+		["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
 		["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
 	}
 end
