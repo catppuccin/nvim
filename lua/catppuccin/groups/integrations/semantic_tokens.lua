98d4e0176aa0bc0f1f945e8ecca1517375ee99e0
Егор Мартынов <48406064+mrtnvgr@users.noreply.github.com>
2023-03-23 19:01:11 +0700

    feat(semantic_tokens): add injected highlights (#435)

--- lua/catppuccin/groups/integrations/semantic_tokens.lua
+++ lua/catppuccin/groups/integrations/semantic_tokens.lua
@@ -12,7 +12,10 @@ function M.get()
 		["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
 		["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
 		["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+		["@lsp.typemod.operator.injected"] = { link = "@operator" },
+		["@lsp.typemod.string.injected"] = { link = "@string" },
 		["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+		["@lsp.typemod.variable.injected"] = { link = "@variable" },
 	}
 end
 
