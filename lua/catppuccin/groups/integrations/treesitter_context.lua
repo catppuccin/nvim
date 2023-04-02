903c31b425c86fedc04743be9e1b1b875fc4147c
mrtnvgr <martynovegorOF@yandex.ru>
2023-04-02 11:37:51 +0700

    fix(nvim-treesitter-context): make `TreesitterContext` more readable when using `transparent_background` option

--- lua/catppuccin/groups/integrations/treesitter_context.lua
+++ lua/catppuccin/groups/integrations/treesitter_context.lua
@@ -2,10 +2,7 @@ local M = {}
 
 function M.get()
 	return {
-		TreesitterContext = {
-			bg = O.transparent_background and C.none or C.mantle,
-			fg = C.text,
-		},
+		TreesitterContext = { bg = C.mantle, fg = C.text },
 		TreesitterContextLineNumber = { fg = C.surface1 },
 	}
 end
