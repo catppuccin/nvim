ec53a2510bcc3c27c4676e740c344c30ffc0f6a9
nullchilly <nullchilly@users.noreply.github.com>
2023-06-09 16:00:56 +0000

    feat: auto-sync upstream palettes

--- lua/catppuccin/palettes/latte.lua
+++ lua/catppuccin/palettes/latte.lua
@@ -1,37 +1,28 @@
--- NOTE: references for Catppuccin Latte
--- monochromatic: https://coolors.co/fbf8f4-e9e6e6-d7d3d9-c4c1cb-b2aebd-a09cb0-8e89a2-7b7794-696487-575279
--- analogous 1: https://coolors.co/de9584-dd7878-ec83d0-822fee-e64553-bb0d33-fe640b
--- analogous 2: https://coolors.co/7287fd-2a6ef5-04a5e5-e49320-209fb5-40a02b-179299
-
-local color_palette = {
+return {
 	rosewater = "#dc8a78",
-	flamingo = "#DD7878",
+	flamingo = "#dd7878",
 	pink = "#ea76cb",
-	mauve = "#8839EF",
-	red = "#D20F39",
-	maroon = "#E64553",
-	peach = "#FE640B",
+	mauve = "#8839ef",
+	red = "#d20f39",
+	maroon = "#e64553",
+	peach = "#fe640b",
 	yellow = "#df8e1d",
-	green = "#40A02B",
+	green = "#40a02b",
 	teal = "#179299",
-	sky = "#04A5E5",
-	sapphire = "#209FB5",
+	sky = "#04a5e5",
+	sapphire = "#209fb5",
 	blue = "#1e66f5",
-	lavender = "#7287FD",
-
-	text = "#4C4F69",
-	subtext1 = "#5C5F77",
-	subtext0 = "#6C6F85",
-	overlay2 = "#7C7F93",
-	overlay1 = "#8C8FA1",
-	overlay0 = "#9CA0B0",
-	surface2 = "#ACB0BE",
-	surface1 = "#BCC0CC",
-	surface0 = "#CCD0DA",
-
-	base = "#EFF1F5",
-	mantle = "#E6E9EF",
-	crust = "#DCE0E8",
+	lavender = "#7287fd",
+	text = "#4c4f69",
+	subtext1 = "#5c5f77",
+	subtext0 = "#6c6f85",
+	overlay2 = "#7c7f93",
+	overlay1 = "#8c8fa1",
+	overlay0 = "#9ca0b0",
+	surface2 = "#acb0be",
+	surface1 = "#bcc0cc",
+	surface0 = "#ccd0da",
+	base = "#eff1f5",
+	mantle = "#e6e9ef",
+	crust = "#dce0e8",
 }
-
-return color_palette
--- lua/catppuccin/palettes/macchiato.lua
+++ lua/catppuccin/palettes/macchiato.lua
@@ -1,37 +1,28 @@
--- NOTE: references for Catppuccin Macchiato
--- monochromatic: https://coolors.co/c5cff5-aeb7da-979fc0-8087a5-696f8a-52576f-3b3f55-24273a-181926-0c0d13
--- analogous 1: https://coolors.co/f4dbd6-f0c6c6-f5bde6-c6a0f6-ee99a0-ed8796-f5a97f
--- analogous 2: https://coolors.co/bfb7e4-8caaee-99d1db-e5c890-85c1dc-a6d189-81c8be
-
-local color_palette = {
-	rosewater = "#F4DBD6",
-	flamingo = "#F0C6C6",
-	pink = "#F5BDE6",
-	mauve = "#C6A0F6",
-	red = "#ED8796",
-	maroon = "#EE99A0",
-	peach = "#F5A97F",
-	yellow = "#EED49F",
-	green = "#A6DA95",
-	teal = "#8BD5CA",
-	sky = "#91D7E3",
-	sapphire = "#7DC4E4",
-	blue = "#8AADF4",
-	lavender = "#B7BDF8",
-
-	text = "#CAD3F5",
-	subtext1 = "#B8C0E0",
-	subtext0 = "#A5ADCB",
-	overlay2 = "#939AB7",
-	overlay1 = "#8087A2",
-	overlay0 = "#6E738D",
-	surface2 = "#5B6078",
-	surface1 = "#494D64",
-	surface0 = "#363A4F",
-
-	base = "#24273A",
-	mantle = "#1E2030",
+return {
+	rosewater = "#f4dbd6",
+	flamingo = "#f0c6c6",
+	pink = "#f5bde6",
+	mauve = "#c6a0f6",
+	red = "#ed8796",
+	maroon = "#ee99a0",
+	peach = "#f5a97f",
+	yellow = "#eed49f",
+	green = "#a6da95",
+	teal = "#8bd5ca",
+	sky = "#91d7e3",
+	sapphire = "#7dc4e4",
+	blue = "#8aadf4",
+	lavender = "#b7bdf8",
+	text = "#cad3f5",
+	subtext1 = "#b8c0e0",
+	subtext0 = "#a5adcb",
+	overlay2 = "#939ab7",
+	overlay1 = "#8087a2",
+	overlay0 = "#6e738d",
+	surface2 = "#5b6078",
+	surface1 = "#494d64",
+	surface0 = "#363a4f",
+	base = "#24273a",
+	mantle = "#1e2030",
 	crust = "#181926",
 }
-
-return color_palette
