d6c45872a3b730e1c7ae3412cd1ba7ce1bd92266
Null Chilly <nullchilly@gmail.com>
2023-07-01 18:06:48 +0700

    fix: setfenv doesn't work for non-cached require

--- lua/catppuccin/lib/mapper.lua
+++ lua/catppuccin/lib/mapper.lua
@@ -1,15 +1,37 @@
 local M = {}
 
-local function get_integrations()
-	local integrations = O["integrations"]
+function M.apply(flavour)
+	flavour = flavour or require("catppuccin").flavour
+
+	local _O, _C, _U = O, C, U -- Borrowing global var (setfenv doesn't work with require)
+	O = require("catppuccin").options
+	C = require("catppuccin.palettes").get_palette(flavour)
+	U = require "catppuccin.utils.colors"
+
+	C.none = "NONE"
+
+	local dim_percentage = O.dim_inactive.percentage
+	C.dim = O.dim_inactive.shade == "dark"
+			and U.vary_color(
+				{ latte = U.darken(C.base, dim_percentage, C.mantle) },
+				U.darken(C.base, dim_percentage, C.mantle)
+			)
+		or U.vary_color(
+			{ latte = U.lighten("#FBFCFD", dim_percentage, C.base) },
+			U.lighten(C.surface0, dim_percentage, C.base)
+		)
+
+	local theme = {}
+	theme.syntax = require("catppuccin.groups.syntax").get()
+	theme.editor = require("catppuccin.groups.editor").get()
 	local final_integrations = {}
 
-	for integration in pairs(integrations) do
+	for integration in pairs(O.integrations) do
 		local cot = false
-		if type(integrations[integration]) == "table" then
-			if integrations[integration]["enabled"] == true then cot = true end
+		if type(O.integrations[integration]) == "table" then
+			if O.integrations[integration].enabled == true then cot = true end
 		else
-			if integrations[integration] == true then cot = true end
+			if O.integrations[integration] == true then cot = true end
 		end
 
 		if cot then
@@ -20,52 +42,19 @@ local function get_integrations()
 			)
 		end
 	end
+	theme.integrations = final_integrations -- plugins
+	theme.terminal = require("catppuccin.groups.terminal").get() -- terminal colors
+	local user_highlights = O.highlight_overrides
+	if type(user_highlights[flavour]) == "function" then user_highlights[flavour] = user_highlights[flavour](C) end
+	theme.custom_highlights = vim.tbl_deep_extend(
+		"keep",
+		user_highlights[flavour] or {},
+		type(user_highlights.all) == "function" and user_highlights.all(C) or user_highlights.all or {}
+	)
 
-	return final_integrations
-end
+	O, C, U = _O, _C, _U -- Returning global var
 
-function M.apply(flavour)
-	flavour = flavour or require("catppuccin").flavour
-
-	local env = {
-		O = require("catppuccin").options,
-		C = require("catppuccin.palattes").get_palette(flavour),
-		U = require "catppuccin.utils.colors",
-	}
-
-	setmetatable(env, { __index = _G })
-
-	local function theme_with_env()
-		C.none = "NONE"
-
-		local dim_percentage = O.dim_inactive.percentage
-		C.dim = O.dim_inactive.shade == "dark"
-				and U.vary_color(
-					{ latte = U.darken(C.base, dim_percentage, C.mantle) },
-					U.darken(C.base, dim_percentage, C.mantle)
-				)
-			or U.vary_color(
-				{ latte = U.lighten("#FBFCFD", dim_percentage, C.base) },
-				U.lighten(C.surface0, dim_percentage, C.base)
-			)
-
-		local theme = {}
-		theme.syntax = require("catppuccin.groups.syntax").get()
-		theme.editor = require("catppuccin.groups.editor").get()
-		theme.integrations = get_integrations() -- plugins
-		theme.terminal = require("catppuccin.groups.terminal").get() -- terminal colors
-		local user_highlights = require("catppuccin").options.highlight_overrides
-		if type(user_highlights[flavour]) == "function" then user_highlights[flavour] = user_highlights[flavour](C) end
-		theme.custom_highlights = vim.tbl_deep_extend(
-			"keep",
-			user_highlights[flavour] or {},
-			type(user_highlights.all) == "function" and user_highlights.all(C) or user_highlights.all or {}
-		)
-
-		return theme
-	end
-
-	return setfenv(theme_with_env, env)()
+	return theme
 end
 
 return M
