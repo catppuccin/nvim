62f67b7f0e4ad3c5e3438bb8c8621acc0026627a
Null Chilly <nullchilly@gmail.com>
2023-07-07 22:56:16 +0700

    test: add more user configs

--- tests/setup_spec.lua
+++ tests/setup_spec.lua
@@ -232,6 +232,299 @@ local configs = {
 			}
 		end,
 	},
+	calops = {
+		integrations = {
+			telescope = true,
+			neotree = {
+				enabled = true,
+				show_root = true,
+				transparent_panel = false,
+			},
+			indent_blankline = {
+				enabled = false,
+				colored_indent_levels = false,
+			},
+			cmp = true,
+			gitsigns = true,
+			notify = true,
+			mini = true,
+			native_lsp = {
+				inlay_hints = {
+					background = false,
+				},
+			},
+		},
+		compile = {
+			enabled = true,
+		},
+		custom_highlights = function()
+			local palette = C
+			local utils = U
+
+			return {
+				NormalFloat = { bg = palette.base },
+				FloatBorder = { fg = palette.mauve },
+				TermFloatBorder = { fg = palette.red },
+
+				TelescopeBorder = { fg = palette.yellow },
+				TelescopePromptBorder = { fg = palette.peach },
+				TelescopePreviewBorder = { fg = palette.teal },
+				TelescopeResultsBorder = { fg = palette.green },
+
+				InclineNormalNC = { bg = palette.surface1, fg = palette.base, blend = 0 },
+				InclineNormal = { bg = palette.overlay1, fg = palette.base, blend = 0 },
+
+				TreesitterContext = { bg = palette.base, style = { "italic" }, blend = 0 },
+				TreesitterContextSeparator = { fg = palette.surface1 },
+
+				DiagnosticUnderlineError = { sp = palette.red, style = { "undercurl" } },
+				DiagnosticUnderlineWarn = { sp = palette.yellow, style = { "undercurl" } },
+				DiagnosticUnderlineInfo = { sp = palette.sky, style = { "undercurl" } },
+				DiagnosticUnderlineHint = { sp = palette.teal, style = { "undercurl" } },
+
+				DiagnosticLineError = { bg = utils.darken(palette.red, 0.095, palette.base) },
+				DiagnosticLineWarn = { bg = utils.darken(palette.yellow, 0.095, palette.base) },
+				DiagnosticLineInfo = { bg = utils.darken(palette.sky, 0.095, palette.base) },
+				DiagnosticLineHint = { bg = utils.darken(palette.teal, 0.095, palette.base) },
+
+				DiagnosticUnnecessary = { sp = palette.mauve, style = { "undercurl" } },
+
+				IndentBlanklineContextChar = { fg = palette.mauve, style = { "nocombine" } },
+
+				ModesInsert = { bg = palette.green },
+				ModesVisual = { bg = palette.mauve },
+
+				LspInlayHint = { fg = palette.surface1 },
+				Comment = { fg = utils.darken(palette.lavender, 0.6) },
+
+				IlluminatedWordText = { bg = palette.surface1, style = { "bold", "underdotted" } },
+				IlluminatedWordWrite = { bg = palette.surface1, style = { "bold", "underdotted" } },
+				IlluminatedWordRead = { bg = palette.surface1, style = { "bold", "underdotted" } },
+
+				UfoVirtText = { fg = palette.base, bg = palette.teal, style = { "bold" } },
+				UfoVirtTextPill = { fg = palette.teal },
+				UfoFoldedBg = { bg = utils.darken(palette.teal, 0.3) },
+				Folded = { bg = palette.base },
+
+				CursorLineSign = { link = "CursorLine" },
+
+				GitSignsAdd = { fg = palette.green, bg = "none" },
+				GitSignsChange = { fg = palette.peach },
+				GitSignsDelete = { fg = palette.red },
+				DiffDeleteVirtLn = { fg = utils.darken(palette.red, 0.3) },
+
+				CustomTabline = { fg = palette.base, bg = palette.surface1 },
+				CustomTablineSel = { fg = palette.base, bg = palette.overlay1 },
+				CustomTablineLogo = { fg = palette.base, bg = palette.mauve },
+				CustomTablinePillIcon = { bg = palette.surface1 },
+				CustomTablinePillIconSel = { bg = palette.surface2 },
+				CustomTablineModifiedIcon = { fg = palette.peach },
+				CustomTablineNumber = { style = { "bold" } },
+
+				VirtColumn = { fg = palette.surface0 },
+
+				CuicuiCharColumn1 = { fg = utils.darken(palette.surface0, 0.8) },
+				CuicuiCharColumn2 = { fg = palette.surface0 },
+
+				CopilotSuggestion = { fg = utils.darken(palette.peach, 0.8), style = { "italic" } },
+
+				MultiCursor = { bg = palette.peach, fg = palette.base },
+				VM_Mono = { bg = palette.peach, fg = palette.base },
+
+				FlashLabel = { bg = palette.peach, fg = palette.base, style = { "bold" } },
+				FlashMatch = { bg = palette.lavender, fg = palette.base },
+				FlashBackdrop = { bg = nil, fg = palette.overlay0, style = { "nocombine" } },
+
+				-- Syntax
+				["@parameter"] = { fg = palette.text, style = { "nocombine" } },
+				["@namespace"] = { fg = palette.pink, style = { "nocombine" } },
+				["@number"] = { fg = palette.green },
+				["@boolean"] = { fg = palette.green, style = { "bold" } },
+				["@type.qualifier"] = { fg = palette.mauve, style = { "bold" } },
+				["@function.macro"] = { fg = palette.blue },
+				["@constant.builtin"] = { fg = palette.green },
+				["@property"] = { fg = utils.brighten(palette.yellow, 0.7) },
+				["@field"] = { fg = utils.brighten(palette.yellow, 0.7) },
+
+				["@lsp.type.struct"] = { fg = palette.yellow },
+				["@lsp.type.property"] = { fg = utils.brighten(palette.yellow, 0.7) },
+				["@lsp.type.interface"] = { fg = palette.peach },
+				["@lsp.type.builtinType"] = { fg = palette.yellow, style = { "bold" } },
+				["@lsp.type.enum"] = { fg = palette.teal },
+				["@lsp.type.enumMember"] = { fg = palette.green },
+				["@lsp.type.variable"] = { fg = palette.text },
+				["@lsp.type.parameter"] = { fg = palette.text },
+				["@lsp.type.namespace"] = { fg = palette.pink },
+				["@lsp.type.number"] = { fg = palette.green },
+				["@lsp.type.boolean"] = { fg = palette.green, style = { "bold" } },
+				["@lsp.type.keyword"] = { fg = palette.mauve },
+				["@lsp.type.decorator"] = { fg = palette.blue },
+				["@lsp.type.unresolvedReference"] = { sp = palette.surface2, style = { "undercurl" } },
+
+				["@lsp.mod.reference"] = { style = { "italic" } },
+				["@lsp.mod.mutable"] = { style = { "bold" } },
+				["@lsp.mod.trait"] = { fg = palette.sapphire },
+				["@lsp.typemod.variable.static"] = { style = { "underdashed" } },
+				["@lsp.typemod.method.defaultLibrary"] = {},
+				["@lsp.typemod.variable.callable"] = { fg = palette.teal },
+			}
+		end,
+	},
+	mrtnvgr = {
+		-- CR01
+		Pmenu = { bg = "" },
+		PmenuSbar = { bg = "" },
+		NormalFloat = { bg = "" },
+	},
+	geril2207 = {
+		flavour = "macchiato",
+		transparent_background = true,
+		highlight_overrides = {
+			all = function(colors)
+				return {
+					DiagnosticSignWarn = { fg = colors.yellow, bg = colors.none },
+					DiagnosticSignHint = { fg = colors.teal, bg = colors.none },
+					DiagnosticSignInfo = { fg = colors.peach, bg = colors.none },
+					DiagnosticSignError = { fg = colors.red, bg = colors.none },
+					DiagnosticVirtualTextWarn = { fg = colors.yellow, bg = colors.none },
+					DiagnosticVirtualTextHint = { fg = colors.teal, bg = colors.none },
+					DiagnosticVirtualTextInfo = { fg = colors.peach, bg = colors.none },
+					DiagnosticVirtualTextError = { fg = colors.red, bg = colors.none },
+				}
+			end,
+		},
+
+		term_colors = false,
+		no_italic = true, -- Force no italic
+		no_bold = true, -- Force no bold
+		styles = {
+			comments = {},
+			conditionals = {},
+			loops = {},
+			functions = {},
+			keywords = {},
+			strings = {},
+			variables = {},
+			numbers = {},
+			booleans = {},
+			properties = {},
+			types = {},
+			operators = {},
+		},
+	},
+	emxxjnm = {
+		flavour = "mocha",
+		transparent_background = true,
+		styles = {
+			keywords = { "bold" },
+			functions = { "italic" },
+		},
+		integrations = {
+			leap = true,
+			mason = true,
+			neotree = true,
+			neotest = true,
+			which_key = true,
+			nvimtree = false,
+			dashboard = false,
+			ts_rainbow = false,
+			dap = { enabled = true, enable_ui = true },
+		},
+		custom_highlights = function(colors)
+			local default = {
+				-- custom
+				PanelHeading = {
+					fg = colors.lavender,
+					bg = true and "NONE" or colors.crust,
+					style = { "bold", "italic" },
+				},
+
+				-- lazy.nvim
+				LazyH1 = {
+					bg = true and "NONE" or colors.peach,
+					fg = true and colors.lavender or colors.base,
+					style = { "bold" },
+				},
+				LazyButton = {
+					bg = "NONE",
+					fg = true and colors.overlay0 or colors.subtext0,
+				},
+				LazyButtonActive = {
+					bg = true and "NONE" or colors.overlay1,
+					fg = true and colors.lavender or colors.base,
+					style = { " bold" },
+				},
+				LazySpecial = { fg = colors.sapphire },
+
+				CmpItemMenu = { fg = colors.subtext1 },
+				MiniIndentscopeSymbol = { fg = colors.overlay0 },
+
+				FloatBorder = {
+					fg = true and colors.overlay1 or colors.mantle,
+					bg = true and "NONE" or colors.mantle,
+				},
+
+				FloatTitle = {
+					fg = colors.subtext0,
+					bg = true and "NONE" or colors.mantle,
+				},
+			}
+
+			local telescope = {
+				TelescopePromptNormal = { bg = colors.crust },
+				TelescopePromptTitle = { fg = colors.subtext0 },
+				TelescopePromptBorder = { bg = colors.crust, fg = colors.crust },
+				TelescopePromptPrefix = { bg = colors.crust, fg = colors.flamingo },
+
+				TelescopeResultsNormal = { bg = colors.mantle },
+				TelescopeResultsTitle = { fg = colors.subtext0 },
+				TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+
+				TelescopePreviewNormal = { bg = colors.crust },
+				TelescopePreviewTitle = { fg = colors.subtext0 },
+				TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+			}
+
+			if not true then return vim.tbl_extend("keep", default, telescope) end
+
+			return default
+		end,
+	},
+	Rishabh672003 = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha
+		background = {
+			-- :h background
+			light = "latte",
+			dark = "mocha",
+		},
+		transparent_background = false,
+		term_colors = false,
+		dim_inactive = {
+			enabled = false,
+			shade = "dark",
+			percentage = 0.15,
+		},
+		no_italic = false, -- Force no italic
+		no_bold = false, -- Force no bold
+		styles = {
+			comments = { "italic" },
+			conditionals = { "italic" },
+		},
+		color_overrides = {},
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			telescope = true,
+			notify = true,
+			mini = false,
+			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+		},
+		custom_highlights = {
+			WhichKeyGroup = { fg = "#FAB387" },
+			WhichKeySeparator = { fg = "#cdd6f4" },
+		},
+	},
 }
 
 describe("setup", function()
