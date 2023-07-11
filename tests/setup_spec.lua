45d22aa5146c703d73bdb5844ff7db0af84b4919
Null Chilly <nullchilly@gmail.com>
2023-07-11 10:00:33 +0700

    feat: add new config

--- tests/setup_spec.lua
+++ tests/setup_spec.lua
@@ -787,6 +787,28 @@ local configs = {
 			return remaps
 		end,
 	},
+	["b-ggs"] = {
+		integrations = {
+			ts_rainbow = true,
+		},
+		color_overrides = {
+			mocha = {
+				text = "#F4CDE9",
+				subtext1 = "#DEBAD4",
+				subtext0 = "#C8A6BE",
+				overlay2 = "#B293A8",
+				overlay1 = "#9C7F92",
+				overlay0 = "#866C7D",
+				surface2 = "#705867",
+				surface1 = "#5A4551",
+				surface0 = "#44313B",
+
+				base = "#352939",
+				mantle = "#211924",
+				crust = "#1a1016",
+			},
+		},
+	},
 }
 
 describe("user", function()
