468b4eaa6b22fd22588a611c0ed96c09ae0d9320
Myles Mo <mylesmo.ash@gmail.com>
2023-07-19 10:22:51 +0800

    feat: add flash.nvim integration

---
+++ lua/catppuccin/groups/integrations/flash.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+function M.get()
+	local bg = O.transparent_background and C.none or C.base
+	return {
+		FlashBackdrop = { fg = C.overlay0 },
+		FlashLabel = { fg = C.green, bg = bg, style = { "bold" } },
+		FlashMatch = { fg = C.lavender, bg = bg },
+		FlashCurrent = { fg = C.peach, bg = bg },
+	}
+end
+
+return M
