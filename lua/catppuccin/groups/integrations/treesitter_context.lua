31b6f0fa35e7515de5e1aace0666807ce929f47a
Yi Ming <ofseed@foxmail.com>
2023-08-05 13:13:13 +0800

    feat(treesitter_context): add highlight for `TreesitterContextLineNumber`

--- lua/catppuccin/groups/integrations/treesitter_context.lua
+++ lua/catppuccin/groups/integrations/treesitter_context.lua
@@ -3,7 +3,12 @@ local M = {}
 function M.get()
 	return O.transparent_background and {
 		TreesitterContextBottom = { sp = C.dim, style = { "underline" } },
-	} or {}
+	} or {
+		TreesitterContextLineNumber = {
+			fg = C.surface1,
+			bg = C.mantle,
+		},
+	}
 end
 
 return M
