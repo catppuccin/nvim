fb61cbfbed67950de58041e5ed4f5483d2452e73
Yi Ming <ofseed@foxmail.com>
2023-08-05 13:26:41 +0800

    feat(treesitter_context): add border bottom for treesitter_context window

--- lua/catppuccin/groups/integrations/treesitter_context.lua
+++ lua/catppuccin/groups/integrations/treesitter_context.lua
@@ -4,6 +4,10 @@ function M.get()
 	return O.transparent_background and {
 		TreesitterContextBottom = { sp = C.dim, style = { "underline" } },
 	} or {
+		TreesitterContextBottom = {
+			sp = C.surface0,
+			style = { "underline" },
+		},
 		TreesitterContextLineNumber = {
 			fg = C.surface1,
 			bg = C.mantle,
