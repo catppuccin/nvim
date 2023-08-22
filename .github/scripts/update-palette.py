36b36be77a5c0f59674cf4b4363e9f80e741193c
winston <hey@winston.sh>
2023-08-22 10:29:30 +0200

    ci: update palette workflow for @catppuccin/palette@v0.3.0
    refs: https://github.com/catppuccin/palette/pull/49

---
+++ .github/scripts/update-palette.py
@@ -0,0 +1,14 @@
+import json
+import urllib.request
+
+url = "https://github.com/catppuccin/palette/raw/main/palette.json"
+
+with urllib.request.urlopen(url) as response:
+    flavors = json.loads(response.read().decode())["flavors"]
+
+for flavor in flavors:
+    with open(f"lua/catppuccin/palettes/{flavor}.lua", "w") as f:
+        f.write("return {\n")
+        for accent in flavors[flavor]:
+            f.write(f'\t{accent} = "{flavors[flavor][accent]["hex"]}",\n')
+        f.write("}\n")
