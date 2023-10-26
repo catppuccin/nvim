632cd72c64a03ec130ca569ea4c69cf62d7697bd
mrtnvgr <root@unixis.fun>
2023-10-26 08:28:31 +0700

    feat(bufferline): support `no_underline` option

--- lua/catppuccin/groups/integrations/bufferline.lua
+++ lua/catppuccin/groups/integrations/bufferline.lua
@@ -104,6 +104,7 @@ function M.get(user_config)
 					color[style] = true
 					if O.no_italic and style == "italic" then color[style] = false end
 					if O.no_bold and style == "bold" then color[style] = false end
+					if O.no_underline and style == "underline" then color[style] = false end
 				end
 			end
 			color.style = nil
