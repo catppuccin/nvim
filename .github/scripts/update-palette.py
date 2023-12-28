674582db197d6b11355d97af4eb51c4ab5d659ca
winston <hey@winston.sh>
2023-12-28 12:10:34 +0100

    chore: update to new format

--- .github/scripts/update-palette.py
+++ .github/scripts/update-palette.py
@@ -4,11 +4,11 @@ import urllib.request
 url = "https://github.com/catppuccin/palette/raw/main/palette.json"
 
 with urllib.request.urlopen(url) as response:
-    flavors = json.loads(response.read().decode())["flavors"]
+    flavors = json.loads(response.read().decode())
 
 for flavor in flavors:
     with open(f"lua/catppuccin/palettes/{flavor}.lua", "w") as f:
         f.write("return {\n")
-        for accent in flavors[flavor]:
-            f.write(f'\t{accent} = "{flavors[flavor][accent]["hex"]}",\n')
+        for color in flavors[flavor]["colors"]:
+            f.write(f'\t{color} = "{flavors[flavor]["colors"][color]["hex"]}",\n')
         f.write("}\n")
