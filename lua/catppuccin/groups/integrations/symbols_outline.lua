d8316b44636d395b63ab5e7e8fff43f878477982
mrtnvgr <root@unixis.fun>
2024-01-20 23:17:05 +0700

    feat: add support for `outline.nvim` plugin

--- lua/catppuccin/groups/integrations/symbols_outline.lua
+++ lua/catppuccin/groups/integrations/symbols_outline.lua
@@ -2,7 +2,11 @@ local M = {}
 
 function M.get()
 	return {
+		-- https://github.com/simrat39/symbols-outline.nvim
 		FocusedSymbol = { fg = C.yellow, bg = C.none },
+
+		-- https://github.com/hedyhli/outline.nvim
+		OutlineCurrent = { link = "FocusedSymbol" },
 	}
 end
 
