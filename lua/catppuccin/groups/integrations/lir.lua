835d980b60d795bb2d8d7ec94338f60a1306fb4b
Simon Pflaumer <s.pflaumer@murena.io>
2024-05-19 13:13:21 +0000

    add lir.nvim (lir-git-status.nvim) integration

---
+++ lua/catppuccin/groups/integrations/lir.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.get()
+  return vim.tbl_deep_extend("force", {}, {
+    LirFloatNormal = { fg = C.text, bg = O.transparent_background and C.none or C.mantle },
+    LirFloatBorder = {
+      fg = O.transparent_background and C.surface1 or C.base,
+      bg = O.transparent_background and C.none or C.base
+    },
+    LirFloatCursorLine = { link = "CursorLine" },
+    LirDir = { fg = C.blue },
+    LirSymLink = { fg = C.pink },
+    LirEmptyDirText = { fg = C.blue },
+    LirFloatCurdirWindowNormal = { fg = C.text },
+    LirFloatCurdirWindowDirName = { fg = C.lavender, style = { "bold" } }
+  }, ((O.integrations.lir and O.integrations.lir.git_status) and {
+    LirGitStatusBracket = { fg = C.overlay0 },
+    LirGitStatusIndex = { fg = C.blue },
+    LirGitStatusWorktree = { fg = C.yellow },
+    LirGitStatusUnmerged = { fg = C.red },
+    LirGitStatusUntracked = { fg = C.subtext0 },
+    LirGitStatusIgnored = { fg = C.subtext0 }
+  }) or {})
+end
+
+return M
