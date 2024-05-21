225ee5b20f45e4769caba5ae7e2fab1b56ac7c27
Myles Mo <mylesmo.ash@gmail.com>
2024-05-21 10:48:11 +0800

    fix:(treesitter-content): respect transparency

--- lua/catppuccin/groups/integrations/treesitter_context.lua
+++ lua/catppuccin/groups/integrations/treesitter_context.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.get()
 	return O.transparent_background and {
 		TreesitterContextBottom = { sp = C.dim, style = { "underline" } },
+		TreesitterContextLineNumber = { fg = C.rosewater },
 	} or {
 		TreesitterContextBottom = {
 			sp = C.surface0,
