1d76b57bb22069a6477030edd68b52f3b07a9e48
vollowx <vollowx@proton.me>
2024-07-07 19:54:00 +0800

    Revert "fix: color float window border background (#722)"
    This reverts commit 31fcfb02c47952d5c75aec893b93b2878abe5fbb, as it
    breaks some of the UI.

--- lua/catppuccin/groups/editor.lua
+++ lua/catppuccin/groups/editor.lua
@@ -38,7 +38,7 @@ function M.get()
 		}, -- normal text in non-current windows
 		NormalSB = { fg = C.text, bg = C.crust }, -- normal text in non-current windows
 		NormalFloat = { fg = C.text, bg = (O.transparent_background and vim.o.winblend == 0) and C.none or C.mantle }, -- Normal text in floating windows.
-		FloatBorder = { fg = C.blue, bg = (O.transparent_background and vim.o.winblend == 0) and C.none or C.mantle },
+		FloatBorder = { fg = C.blue },
 		FloatTitle = { fg = C.subtext0 }, -- Title of floating windows
 		Pmenu = {
 			bg = (O.transparent_background and vim.o.pumblend == 0) and C.none or U.darken(C.surface0, 0.8, C.crust),
