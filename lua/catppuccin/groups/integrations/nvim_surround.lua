2089a0d533e4d83ab5cef10aaa9f7861eae5f0ca
Myles Mo <mylesmo.ash@gmail.com>
2024-11-05 16:06:35 +0800

    fix(nvim-surround): bg conflicts with document highlight

--- lua/catppuccin/groups/integrations/nvim_surround.lua
+++ lua/catppuccin/groups/integrations/nvim_surround.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.get()
 	return {
-		NvimSurroundHighlight = { bg = U.darken(C.peach, 0.6, C.base), style = { "bold" } },
+		NvimSurroundHighlight = { sp = C.peach, style = { "underline" } },
 	}
 end
 
