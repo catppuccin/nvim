8a54e1b03ed5266a33ee431c95d2d71fcbd77fbb
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2025-04-03 18:35:56 +0530

    fix again

--- lua/catppuccin/groups/integrations/telescope.lua
+++ lua/catppuccin/groups/integrations/telescope.lua
@@ -4,46 +4,40 @@ function M.get()
 	if O.integrations.telescope.style == "nvchad" then
 		return {
 			TelescopeBorder = {
-				fg = O.transparent_background and C.blue or C.mantle,
-				bg = O.transparent_background and C.none or C.mantle,
+				fg = C.blue,
+				bg = C.mantle,
 			},
 			TelescopeMatching = { fg = C.blue },
 			TelescopeNormal = {
-				bg = O.transparent_background and C.none or C.mantle,
+				bg = C.mantle,
 			},
 			TelescopePromptBorder = {
-				fg = O.transparent_background and C.blue or C.surface0,
-				bg = O.transparent_background and O.integrations.telescope.style == "nvchad" and C.surface0
-					or C.none
-					or C.surface0,
+				fg = C.blue,
+				bg = C.surface0,
 			},
 			TelescopePromptNormal = {
 				fg = C.text,
-				bg = O.transparent_background and O.integrations.telescope.style == "nvchad" and C.surface0
-					or C.none
-					or C.surface0,
+				bg = C.surface0,
 			},
 			TelescopePromptPrefix = {
 				fg = C.flamingo,
-				bg = O.transparent_background and O.integrations.telescope.style == "nvchad" and C.surface0
-					or C.none
-					or C.surface0,
+				bg = C.surface0,
 			},
 			TelescopePreviewTitle = {
-				fg = O.transparent_background and C.green or C.base,
-				bg = O.transparent_background and C.none or C.green,
+				fg = C.base,
+				bg = C.green,
 			},
 			TelescopePromptTitle = {
-				fg = O.transparent_background and C.red or C.base,
-				bg = O.transparent_background and C.none or C.red,
+				fg = C.base,
+				bg = C.red,
 			},
 			TelescopeResultsTitle = {
-				fg = O.transparent_background and C.lavender or C.mantle,
-				bg = O.transparent_background and C.none or C.lavender,
+				fg = C.mantle,
+				bg = C.lavender,
 			},
 			TelescopeSelection = {
-				fg = O.transparent_background and C.flamingo or C.text,
-				bg = O.transparent_background and C.none or C.surface0,
+				fg = C.text,
+				bg = C.surface0,
 				style = { "bold" },
 			},
 			TelescopeSelectionCaret = { fg = C.flamingo },
