d4c546073ae58275a96021cdad90506d600a9c48
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2025-05-04 17:58:28 +0200

    fix(cursor): update cursor color to rosewater / base
    Follow the style guide at
    https://github.com/catppuccin/catppuccin/blob/main/docs/style-guide.md.
    
    Ports for popular terminals follow this style, so nvim, being a
    primarily terminal app, should strive the same. Examples:
    
    - [iTerm2 cursor](https://github.com/catppuccin/iterm/blob/b2936a6e55270fcc55421b1bb7d5fd194a489591/build.ts#L50)
    - [Ghostty cursor](https://github.com/catppuccin/ghostty/blob/9e38fc2b4e76d4ed5ff9edc5ac9e4081c7ce6ba6/ghostty.tera#L26)
    - [Alacritty cursor](https://github.com/catppuccin/alacritty/blob/f6cb5a5c2b404cdaceaff193b9c52317f62c62f7/alacritty.tera#L17)

--- lua/catppuccin/groups/editor.lua
+++ lua/catppuccin/groups/editor.lua
@@ -4,9 +4,9 @@ function M.get()
 	return {
 		ColorColumn = { bg = C.surface0 }, -- used for the columns set with 'colorcolumn'
 		Conceal = { fg = C.overlay1 }, -- placeholder characters substituted for concealed text (see 'conceallevel')
-		Cursor = { fg = C.base, bg = C.text }, -- character under the cursor
-		lCursor = { fg = C.base, bg = C.text }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
-		CursorIM = { fg = C.base, bg = C.text }, -- like Cursor, but used when in IME mode |CursorIM|
+		Cursor = { fg = C.base, bg = C.rosewater }, -- character under the cursor
+		lCursor = { fg = C.base, bg = C.rosewater }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
+		CursorIM = { fg = C.base, bg = C.rosewater }, -- like Cursor, but used when in IME mode |CursorIM|
 		CursorColumn = { bg = C.mantle }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
 		CursorLine = {
 			bg = U.vary_color({ latte = U.lighten(C.mantle, 0.70, C.base) }, U.darken(C.surface0, 0.64, C.base)),
