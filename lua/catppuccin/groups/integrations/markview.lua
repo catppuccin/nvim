56a9dfd1e05868cf3189369aad87242941396563
robin <67917529+comfysage@users.noreply.github.com>
2025-05-26 16:46:16 +0200

    fix: transparent markdown headings bg (#861)

--- lua/catppuccin/groups/integrations/markview.lua
+++ lua/catppuccin/groups/integrations/markview.lua
@@ -4,7 +4,7 @@ local M = {}
 -- https://github.com/OXY2DEV/markview.nvim#-highlight-groups
 
 function M.get()
-	local darkening_percentage = O.transparent_background and 0.28 or 0.095
+	local darkening_percentage = O.transparent_background and U.vary_color({ latte = 0.15 }, 0.28) or 0.095
 
 	local blockquote_bg = not O.transparent_background and C.mantle or nil
 
@@ -32,11 +32,10 @@ function M.get()
 	}
 
 	local syntax = require("catppuccin.groups.syntax").get()
-	local base = not O.transparent_background and C.base or nil
 
 	for i = 0, 7 do
 		local color = rainbow[i] or syntax["rainbow" .. i].fg
-		local bg = U.darken(color, darkening_percentage, base)
+		local bg = U.darken(color, darkening_percentage, C.base)
 		groups["MarkviewPalette" .. i] = { fg = color, bg = bg }
 		groups["MarkviewPalette" .. i .. "Fg"] = { fg = color }
 		groups["MarkviewPalette" .. i .. "Bg"] = { bg = bg }
