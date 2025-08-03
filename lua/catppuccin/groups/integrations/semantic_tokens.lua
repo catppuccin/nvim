42d33b5ac45a39f9f63f6580e0fff94c560597d8
uncenter <uncenter@uncenter.dev>
2025-08-03 10:39:47 -0400

    fix(integrations/semantic_tokens): unset
    We can reset to Neovim's defaults now, which link to Tree-sitter captures. This file as a whole may not be necessary but keeping it in case we want to add overrides in the future.

--- lua/catppuccin/groups/integrations/semantic_tokens.lua
+++ lua/catppuccin/groups/integrations/semantic_tokens.lua
@@ -12,36 +12,6 @@ If you want to stay on nvim 0.7, disable the integration.
 		return {}
 	end
 	return {
-		["@lsp.type.boolean"] = { link = "@boolean" },
-		["@lsp.type.builtinType"] = { link = "@type.builtin" },
-		["@lsp.type.comment"] = { link = "@comment" },
-		["@lsp.type.enum"] = { link = "@type" },
-		["@lsp.type.enumMember"] = { link = "@constant" },
-		["@lsp.type.escapeSequence"] = { link = "@string.escape" },
-		["@lsp.type.formatSpecifier"] = { link = "@punctuation.special" },
-		["@lsp.type.interface"] = { fg = C.flamingo },
-		["@lsp.type.keyword"] = { link = "@keyword" },
-		["@lsp.type.namespace"] = { link = "@module" },
-		["@lsp.type.number"] = { link = "@number" },
-		["@lsp.type.operator"] = { link = "@operator" },
-		["@lsp.type.parameter"] = { link = "@parameter" },
-		["@lsp.type.property"] = { link = "@property" },
-		["@lsp.type.selfKeyword"] = { link = "@variable.builtin" },
-		["@lsp.type.typeAlias"] = { link = "@type.definition" },
-		["@lsp.type.unresolvedReference"] = { link = "@error" },
-		["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
-		["@lsp.typemod.class.defaultLibrary"] = { link = "@type.builtin" },
-		["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" },
-		["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" },
-		["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
-		["@lsp.typemod.keyword.async"] = { link = "@keyword.coroutine" },
-		["@lsp.typemod.macro.defaultLibrary"] = { link = "@function.builtin" },
-		["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
-		["@lsp.typemod.operator.injected"] = { link = "@operator" },
-		["@lsp.typemod.string.injected"] = { link = "@string" },
-		["@lsp.typemod.type.defaultLibrary"] = { link = "@type.builtin" },
-		["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
-		["@lsp.typemod.variable.injected"] = { link = "@variable" },
 	}
 end
 
