90efaa0decdf09a668cfc55a45404e446285fb10
uncenter <uncenter@uncenter.dev>
2025-08-03 13:46:44 -0400

    chore: move comment to right place

--- lua/catppuccin/groups/integrations/semantic_tokens.lua
+++ lua/catppuccin/groups/integrations/semantic_tokens.lua
@@ -9,12 +9,12 @@ If you want to stay on nvim 0.7, disable the integration.
 ]],
 			vim.log.levels.INFO
 		)
-		return {
-			-- https://neovim.io/doc/user/lsp.html#lsp-semantic-highlight
-			-- Semantic tokens (@lsp.type.<...>) are no longer defined in this file, as Neovim links to already themed Tree-sitter captures by default.
-		}
+		return {}
 	end
-	return {}
+	return {
+		-- https://neovim.io/doc/user/lsp.html#lsp-semantic-highlight
+		-- Semantic tokens (@lsp.type.<...>) are no longer defined in this file, as Neovim links to already themed Tree-sitter captures by default.
+	}
 end
 
 return M
