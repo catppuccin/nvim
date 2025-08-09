168ba86a509a7ed7e077623538b1235a83ecf9d0
Mayro <may.ronen9@gmail.com>
2025-08-09 11:39:55 +0300

    refactor: use vim.iter instead of tables

--- lua/catppuccin/lib/detect_integrations.lua
+++ lua/catppuccin/lib/detect_integrations.lua
@@ -74,20 +74,16 @@ local integration_mappings = {
 	["which-key.nvim"]									=	"which_key",
 }
 
-local installed_plugins = {}
-if pcall(require, "lazy") then
-	for plugin, _ in pairs(require("lazy.core.config").plugins) do
-		-- special case for the "mini" library, if one module is present, mark as if the whole library is installed
-		if plugin:match "mini.*" then
-			if not vim.tbl_contains(installed_plugins, "mini.nvim") then
-				table.insert(installed_plugins, "mini.nvim")
-			end
-		else
-			table.insert(installed_plugins, plugin)
-		end
-	end
+
+if vim.fn.has "nvim-0.12.0" == 1 then
+	installed_plugins = vim.iter(vim.pack.get()):map(function(plug) return plug.spec.name end)
 end
 
+if pcall(require, "pckr") then installed_plugins = vim.iter(require("pckr.plugin").plugins_by_name) end
+
+if pcall(require, "lazy") then installed_plugins = vim.iter(require("lazy.core.config").plugins) end
+
+assert(installed_plugins ~= nil, "must be populated by one of supported plugin managers")
 function M.create_integrations_table()
 	local integrations = {}
 	local ctp_defaults = require("catppuccin").default_options.integrations
