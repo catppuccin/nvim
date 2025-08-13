381e605908efdf321887f962aca51b64f42bb401
robin <comfysage@isabelroses.com>
2025-08-13 16:02:53 +0200

    fix(integrations/colorful_winsep): update hl name (#913)
    * fix(integrations/colorful_winsep): update hl name
    
    fixes #912
    
    <https://github.com/nvim-zh/colorful-winsep.nvim/commit/642afcd59d900b2ab2493fa7f03e64a1f7eeb8b5>
    
    * fix: add legacy highlight link

--- lua/catppuccin/groups/integrations/colorful_winsep.lua
+++ lua/catppuccin/groups/integrations/colorful_winsep.lua
@@ -2,10 +2,11 @@ local M = {}
 
 function M.get()
 	return {
-		NvimSeparator = {
+		ColorfulWinSep = {
 			fg = C[O.integrations.colorful_winsep.color],
 			bg = O.transparent_background and C.none or C.base,
 		},
+		NvimSeparator = { link = "ColorfulWinSep" },
 	}
 end
 
