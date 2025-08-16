49e173995748f86aaa9363508c676eb6cacbfc46
robin <comfysage@isabelroses.com>
2025-08-16 12:51:18 +0200

    refactor: add `detect_plugins` fn

--- lua/catppuccin/lib/detect_integrations.lua
+++ lua/catppuccin/lib/detect_integrations.lua
@@ -11,33 +11,39 @@ assert(
 	"before using integration_mappings generate it using the script: `./scripts/generate_integration_mappings_table.lua`"
 )
 
-local installed_plugins = {}
+function M.detect_plugins()
+	local installed_plugins = {}
 
-if vim.fn.has "nvim-0.12.0" == 1 then
-	vim.list_extend(
-		installed_plugins,
-		vim.iter(vim.pack.get()):map(function(plugin) return plugin.spec.name end):totable()
-	)
+	if vim.fn.has "nvim-0.12.0" == 1 then
+		vim.list_extend(
+			installed_plugins,
+			vim.iter(vim.pack.get()):map(function(plugin) return plugin.spec.name end):totable()
+		)
+	end
+
+	if pcall(require, "pckr") then vim.list_extend(installed_plugins, require("pckr.plugin").plugins_by_name) end
+
+	if pcall(require, "lazy") then vim.list_extend(installed_plugins, require("lazy.core.config").plugins) end
+
+	local seen = {}
+
+	installed_plugins = vim.iter(installed_plugins)
+		:map(function(plugin_name)
+			if string.sub(plugin_name, 0, 5) == "mini." then return "mini.nvim" end
+
+			return plugin_name
+		end)
+		:filter(function(plugin_name)
+			if seen[plugin_name] then return false end
+			seen[plugin_name] = true
+			return true
+		end)
+		:totable()
+
+	return installed_plugins
 end
 
-if pcall(require, "pckr") then vim.list_extend(installed_plugins, require("pckr.plugin").plugins_by_name) end
-
-if pcall(require, "lazy") then vim.list_extend(installed_plugins, require("lazy.core.config").plugins) end
-
-local seen = {}
-
-installed_plugins = vim.iter(installed_plugins)
-	:map(function(plugin_name)
-		if string.sub(plugin_name, 0, 5) == "mini." then return "mini.nvim" end
-
-		return plugin_name
-	end)
-	:filter(function(plugin_name)
-		if seen[plugin_name] then return false end
-		seen[plugin_name] = true
-		return true
-	end)
-	:totable()
+local installed_plugins = M.detect_plugins()
 
 function M.create_integrations_table()
 	local integrations = {}
