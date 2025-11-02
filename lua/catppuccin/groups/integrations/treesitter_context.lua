6cae84882f5a1bf1f4aca8477731e1f3195be2c8
robin <comfysagedev@gmail.com>
2025-11-02 11:12:05 +0100

    fix(treesitter_context): context follows normal transparency

--- lua/catppuccin/groups/integrations/treesitter_context.lua
+++ lua/catppuccin/groups/integrations/treesitter_context.lua
@@ -3,21 +3,22 @@ local M = {}
 M.url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
 
 function M.get()
-	return O.transparent_background
-			and {
-				TreesitterContextBottom = { sp = C.dim, style = { "underline" } },
-				TreesitterContextLineNumber = { fg = C.rosewater },
-			}
-		or {
-			TreesitterContextBottom = {
-				sp = C.surface0,
-				style = { "underline" },
-			},
-			TreesitterContextLineNumber = {
-				fg = C.surface1,
-				bg = C.mantle,
-			},
-		}
+	return {
+		TreesitterContext = {
+			fg = C.text,
+			bg = O.transparent_background and C.none or C.mantle,
+		},
+		TreesitterContextBottom = {
+			sp = O.transparent_background and C.dim or C.surface0,
+			style = { "underline" },
+		},
+		TreesitterContextLineNumber = O.transparent_background and {
+			fg = C.rosewater,
+		} or {
+			fg = C.surface1,
+			bg = C.mantle,
+		},
+	}
 end
 
 return M
