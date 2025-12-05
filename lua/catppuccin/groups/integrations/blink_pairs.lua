a9dc93ecbfaf760a6c9da904aa4a8a0af737d7ce
robin <comfysagedev@gmail.com>
2025-12-05 15:41:36 +0100

    fix: pairs colors
    support a similar amount of colors as other plugins like rainbow-delimiters and use the correct colors for their names (e.g. `"blue" != "skye"`).

--- lua/catppuccin/groups/integrations/blink_pairs.lua
+++ lua/catppuccin/groups/integrations/blink_pairs.lua
@@ -4,9 +4,13 @@ M.url = "https://github.com/saghen/blink.pairs"
 
 function M.get()
 	return {
-		BlinkPairsBlue = { fg = C.sky },
-		BlinkPairsOrange = { fg = C.flamingo },
+		BlinkPairsBlue = { fg = C.blue },
+		BlinkPairsOrange = { fg = C.peach },
 		BlinkPairsPurple = { fg = C.mauve },
+		BlinkPairsRed = { fg = C.red },
+		BlinkPairsYellow = { fg = C.yellow },
+		BlinkPairsGreen = { fg = C.green },
+		BlinkPairsCyan = { fg = C.teal },
 		BlinkPairsUnmatched = { fg = C.red },
 		BlinkPairsMatchParen = { fg = C.yellow, style = { "bold" } },
 	}
