213df6807f2b1519254e18057e5032a84c97a12d
robin <comfysagedev@gmail.com>
2025-12-05 16:05:36 +0100

    style: fix fmt

--- lua/catppuccin/groups/integrations/blink_pairs.lua
+++ lua/catppuccin/groups/integrations/blink_pairs.lua
@@ -5,12 +5,12 @@ M.url = "https://github.com/saghen/blink.pairs"
 function M.get()
 	return {
 		BlinkPairsRed = { fg = C.red },
-    BlinkPairsYellow = { fg = C.yellow },
-    BlinkPairsBlue = { fg = C.blue },
-    BlinkPairsOrange = { fg = C.peach },
-    BlinkPairsGreen = { fg = C.green },
-    BlinkPairsPurple = { fg = C.mauve },
-    BlinkPairsCyan = { fg = C.teal },
+		BlinkPairsYellow = { fg = C.yellow },
+		BlinkPairsBlue = { fg = C.blue },
+		BlinkPairsOrange = { fg = C.peach },
+		BlinkPairsGreen = { fg = C.green },
+		BlinkPairsPurple = { fg = C.mauve },
+		BlinkPairsCyan = { fg = C.teal },
 		BlinkPairsUnmatched = { fg = C.red },
 		BlinkPairsMatchParen = { link = "MatchParen" },
 	}
